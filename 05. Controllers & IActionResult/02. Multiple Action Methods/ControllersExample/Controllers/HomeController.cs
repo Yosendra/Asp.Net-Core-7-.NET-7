4711705f0fc32f45de49544b08ba47e2002d912d
Yosendra <yosendra.evri@gmail.com>
2023-12-10 10:26:24 +0800

    Controller - 02. Multiple Action Methods

---
+++ 05. Controllers & IActionResult/02. Multiple Action Methods/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController
+    {
+        [Route("home")]
+        [Route("/")]    // we can map action method to more than one path, so when root path accessed this method will be also invoked other than the path 'home'
+        public string Index()   // by convention, first action method named 'Index()'
+        {
+            return "Hello from Index";
+        }
+        
+        [Route("about")]
+        public string About()
+        {
+            return "Hello from About";
+        }
+        
+        [Route("contact-us")]   // notice the difference between 'contact-us' and 'Contact', both are independent individual things so it is possible to be different
+        public string Contact()
+        {
+            return "Hello from Contact";
+        }
+    }
+}
