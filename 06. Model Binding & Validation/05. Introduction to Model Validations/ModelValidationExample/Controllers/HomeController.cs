8b16ae71461182ef97e440ba909f79be22f7e643
Yosendra <yosendra.evri@gmail.com>
2023-12-13 22:42:17 +0800

    Model Binding & Validation - 05. Introduction to Model Validations

---
+++ 06. Model Binding & Validation/05. Introduction to Model Validations/ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace ModelValidationExample.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.Models;
+
+public class HomeController : Controller
+{
+    // Request from Postman with POST method to url '/register'
+    // Request Body
+    //   Name : scott
+    //   Email : scott@example.com
+    //   Phone : 123456
+    //   Password : scott123
+    //   Confirm Password : abc123
+    //   Price : -10 
+
+    [Route("register")]
+    // Instead we put the validation on controller
+    //  we can put the validation in model class
+    public IActionResult Index(Person person)
+    {
+        return Content($"{person}");
+    }
+}
---
+++ 06. Model Binding & Validation/05. Introduction to Model Validations/ModelValidationExample/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace ModelValidationExample.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Person
+{
+    // Process
+    // Request -> Model Binding -> Model Validation
+    //  -> Controller (receive request data as model object,
+    //                  send response data as model object)
+    //  -> Execute Action Result
+    //  -> Response
+
+    // Here in model class, we put the validation attribute above the property we want validate
+    [Required]
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Password { get; set; }
+    public string? ConfirmPassword { get; set; }
+    public double? Price { get; set; }
+    public override string ToString()
+    {
+        return $"Person object\n" +
+               $"Name: {Name}\n" +
+               $"Email: {Email}\n" +
+               $"Phone: {Phone}\n" +
+               $"Password: {Password}\n" +
+               $"Confirm Password: {ConfirmPassword}\n" +
+               $"Price: {Price}\n";
+    }
+}
