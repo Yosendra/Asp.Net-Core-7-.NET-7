66b4decdb91a0e74b96edec5d5d0972a1627ed45
Yosendra <yosendra.evri@gmail.com>
2023-12-15 16:53:43 +0800

    Model Binding & Validation - 07. All Model Validations - Part 1

---
+++ 06. Model Binding & Validation/07. All Model Validations - Part 1/ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿namespace ModelValidationExample.Controllers;
+
+
+using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.Models;
+
+public class HomeController : Controller
+{
+    // Request from Postman with POST method to url '/register'
+    // Request Body
+    //   Name : scott
+    //   Email : scott@example.com
+    //   Phone : 123456
+    //   Password : scott123
+    //   Confirm Password : abc123
+    //   Price : -10 
+
+    [Route("register")]
+    public IActionResult Index(Person person)
+    {
+        if (!ModelState.IsValid)
+        {
+            List<string> errorList = new();
+
+            errorList = ModelState.Values
+                .SelectMany(value => value.Errors)
+                .Select(error => error.ErrorMessage).ToList();
+
+            string errorMessage = string.Join('\n', errorList);
+            return BadRequest(errorMessage);
+        }
+
+        return Content($"{person}");
+    }
+}
---
+++ 06. Model Binding & Validation/07. All Model Validations - Part 1/ModelValidationExample/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace ModelValidationExample.Models;
+
+// Model Validation Attributes
+//  [Required(ErrorMessage = "your message")]
+//    Specifies that the property value is required (can't be blank or empty)
+//
+//  [StringLength(int maximumLength, MinimumLength = value, ErrorMessage = "value")]
+//    Specifies minimum and maximum length (number of characters) allowed in the string.
+//
+//  [Range(int minimum, int maximum, ErrorMessage = "value")]
+//    Specifies minimum and maximum numerical value allowed.
+
+using System.ComponentModel.DataAnnotations;
+
+public class Person
+{
+    [Required(ErrorMessage = "{0} can't be empty or null")] // {0} -> Property name, which is 'Name'
+    [Display(Name = "Person Name")] // This attribute is to custom property displayed in error message response
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Password { get; set; }
+    public string? ConfirmPassword { get; set; }
+
+    [Range(0, 999.99, ErrorMessage = "{0} should be between {1} and {2}")]
+    public double? Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"Person object\n" +
+               $"Name: {Name}\n" +
+               $"Email: {Email}\n" +
+               $"Phone: {Phone}\n" +
+               $"Password: {Password}\n" +
+               $"Confirm Password: {ConfirmPassword}\n" +
+               $"Price: {Price}";
+    }
+}
