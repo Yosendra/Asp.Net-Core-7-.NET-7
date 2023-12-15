1b4b0aa64ec2e4cd0db87462adb24f019da9e0b1
Yosendra <yosendra.evri@gmail.com>
2023-12-15 21:39:04 +0800

    Model Binding & Validation - 09. Custom Validation

---
+++ 06. Model Binding & Validation/09. Custom Validation/ModelValidationExample/CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationExample.CustomValidators;
+
+public class MinimumYearValidatorAttribute : ValidationAttribute    // must inherit from this class
+{
+    public int MinimumYear { get; set; } = 2000;
+    public string DefaultErrorMessage { get; set; } = "Year should less than {0}";
+
+    public MinimumYearValidatorAttribute()
+    {
+        
+    }
+
+    // Create parameterized constructor to enable passing argument in the attribute
+    public MinimumYearValidatorAttribute(int minimumYear)
+    {
+        MinimumYear = minimumYear;
+    }
+
+    // override this method
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not null)
+        {
+            DateTime date = (DateTime)value;
+            if (date.Year > MinimumYear)
+                // ErrorMessage is from the parent
+                // If attribute do not provide 'minimumYear' argument, use our custom 'DefaultErrorMessage' message
+                return new ValidationResult(string.Format(ErrorMessage ?? DefaultErrorMessage, MinimumYear));
+            else
+                return ValidationResult.Success;
+        }
+
+        return null; // no validation
+    }
+}
---
+++ 06. Model Binding & Validation/09. Custom Validation/ModelValidationExample/Models/Person.cs
@@ -0,0 +1,54 @@
+﻿namespace ModelValidationExample.Models;
+
+// Custom Validation Attributes
+//  look at 'MinimumYearValidatorAttribute.cs'
+
+using System.ComponentModel.DataAnnotations;
+using ModelValidationExample.CustomValidators;
+
+public class Person
+{
+    [Required(ErrorMessage = "{0} can't be empty or null")] // {0} -> Property name, which is 'Name'
+    [Display(Name = "Person Name")] // This attribute is to custom property displayed in error message response
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
+    [RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} should contain only alphabets, space, and dot (.)")]
+    public string? Name { get; set; }
+
+
+    [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
+    [Required(ErrorMessage = "{0} can't be blank")]
+    public string? Email { get; set; }
+
+
+    [Phone(ErrorMessage = "{0} should contain 10 digits")]
+    public string? Phone { get; set; }
+
+
+    [Required(ErrorMessage = "{0} can't be blank")]
+    public string? Password { get; set; }
+
+
+    [Required(ErrorMessage = "{0} can't be blank")]
+    [Compare("Password", ErrorMessage = "{0} and {1} do not match")]
+    [Display(Name = "Re-enter Password")]
+    public string? ConfirmPassword { get; set; }
+
+
+    [Range(0, 999.99, ErrorMessage = "{0} should be between {1} and {2}")]
+    public double? Price { get; set; }
+
+
+    [MinimumYearValidator(2002, ErrorMessage = "Maximum date of birth year is {0}")]
+    public DateTime? DateOfBirth { get; set; }
+
+    public override string ToString()
+    {
+        return $"Person object\n" +
+               $"Name: {Name}\n" +
+               $"Email: {Email}\n" +
+               $"Phone: {Phone}\n" +
+               $"Password: {Password}\n" +
+               $"Confirm Password: {ConfirmPassword}\n" +
+               $"Price: {Price}";
+    }
+}
