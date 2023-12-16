911b3947417c307d64d8b77371a57c8a0af47cfe
Yosendra <yosendra.evri@gmail.com>
2023-12-16 20:18:00 +0800

    Model Binding & Validation - 15. Custom Model Binders

---
+++ 06. Model Binding & Validation/15. Custom Model Binders/ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.CustomModelBinders;
+using ModelValidationExample.Models;
+
+// Custom Model Binder
+//  Case : in the request body there are FirstName and LastName values sent,
+//         we want to bind them into single property 'Name'
+
+namespace ModelValidationExample.Controllers;
+
+public class HomeController : Controller
+{
+    // Request from Postman with POST method to url '/register'
+    // Request Body (form field)
+    //   FirstName : Scott
+    //   LastName : Pilgrim
+    //   Email : scott@example.com
+    //   Phone : 123456
+    //   Password : scott123
+    //   ConfirmPassword : scott123
+    //   Price : 1
+    //   DateOfBirth : 2001-06-01
+    //   FromDate : 2001-06-01
+    //   ToDate : 2001-06-04
+
+    // Notice the validation with attribute in model is still working as it is
+
+    [Route("register")]
+    public IActionResult Index([FromBody] // Use the our custom ModelBinder. our responsibility
+                                          // is reading the value provider, then create and return the object model
+                               [ModelBinder(BinderType = typeof(PersonModelBinder))] Person person)
+    {
+        if (!ModelState.IsValid)
+        {
+            List<string> errorList = new();
+
+            errorList = ModelState.Values
+                .SelectMany(value => value.Errors)
+                .Select(error => error.ErrorMessage).ToList();
+
+            string errorMessage = string.Join('\n', errorList);
+            return BadRequest(errorMessage);
+        }
+
+        return Content($"{person}");
+    }
+}
---
+++ 06. Model Binding & Validation/15. Custom Model Binders/ModelValidationExample/Models/Person.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ModelValidationExample.CustomValidators;
+
+namespace ModelValidationExample.Models;
+
+public class Person : IValidatableObject
+{
+    [Required(ErrorMessage = "{0} can't be empty or null")]
+    [Display(Name = "Person Name")]
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
+    [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only alphabets, space, and dot (.)")]
+    public string? Name { get; set; }
+
+
+    [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
+    [Required(ErrorMessage = "{0} can't be blank")]
+    public string? Email { get; set; }
+
+
+    [Phone(ErrorMessage = "{0} should contain 10 digits")]
+    public string? Phone { get; set; }
+
+
+    [Required(ErrorMessage = "{0} can't be blank")]
+    public string? Password { get; set; }
+
+
+    [Required(ErrorMessage = "{0} can't be blank")]
+    [Compare("Password", ErrorMessage = "{0} and {1} do not match")]
+    [Display(Name = "Re-enter Password")]
+    public string? ConfirmPassword { get; set; }
+
+
+    [Range(0, 999.99, ErrorMessage = "{0} should be between {1} and {2}")]
+    public double? Price { get; set; }
+
+
+    [MinimumYearValidator(2002, ErrorMessage = "Maximum date of birth year is {0}")]
+    public DateTime? DateOfBirth { get; set; }
+
+    public DateTime? FromDate { get; set; }
+
+    [DateRangeValidator("FromDate", ErrorMessage = "'FromDate' should be older than or equal to 'ToDate'")]
+    public DateTime? ToDate { get; set; }
+
+    public int? Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"Person object\n" +
+               $"Name: {Name}\n" +
+               $"Email: {Email}\n" +
+               $"Phone: {Phone}\n" +
+               $"Password: {Password}\n" +
+               $"Confirm Password: {ConfirmPassword}\n" +
+               $"Price: {Price}";
+    }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (DateOfBirth.HasValue && Age.HasValue)
+        {
+            yield return new ValidationResult("Either of Date of Birth or Age must be supplied, not both", new[] { nameof(Age) });
+        }
+    }
+}
