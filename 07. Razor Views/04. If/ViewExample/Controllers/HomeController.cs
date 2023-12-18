88dd682c4f1353d255b00fdc9a3c563fecf66ada
Yosendra <yosendra.evri@gmail.com>
2023-12-18 16:24:49 +0800

    Razor Views - 04. If

---
+++ 07. Razor Views/04. If/ViewExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Razor - if                       Razor - if..else
+//
+// @if (condition) {                @if (condition) {
+//    C# / html code here              C# / html code here
+// }                                }
+//                                  else {
+//                                     C# / html code here
+//                                  }
+
+namespace ViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("home")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ 07. Razor Views/04. If/ViewExample/Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace ViewExample.Models;
+
+public class Person
+{
+    public string? Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+}
