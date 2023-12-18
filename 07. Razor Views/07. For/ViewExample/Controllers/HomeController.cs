90043b029cdf8b257088341e7f0f61be5f45d64c
Yosendra <yosendra.evri@gmail.com>
2023-12-18 17:17:20 +0800

    Razor Views - 07. For

---
+++ 07. Razor Views/07. For/ViewExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Razor - for
+//
+// @for (initialization; condition; iteration) {
+// 
+//   C# / html code here
+//   
+// }
+
+namespace ViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("home")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
