1a433155a6ba79cd7fa889767ad4a70a1b96c4bb
Yosendra <yosendra.evri@gmail.com>
2023-12-18 17:07:35 +0800

    Razor Views - 06. Foreach

---
+++ 07. Razor Views/06. Foreach/ViewExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Razor - foreach
+//
+// @foreach (var item in collection) {
+// 
+//   C# / html code here
+//   
+// }
+
+namespace ViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("home")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
