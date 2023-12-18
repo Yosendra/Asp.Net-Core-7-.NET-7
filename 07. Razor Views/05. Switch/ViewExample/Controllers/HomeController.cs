b47a3054d1524d837942e864bf2f7e3b6ccc67fe
Yosendra <yosendra.evri@gmail.com>
2023-12-18 16:42:32 +0800

    Razor Views - 05. Switch

---
+++ 07. Razor Views/05. Switch/ViewExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Razor - Switch
+//
+// @switch (variable) {
+//
+//   case value1: C# / html code here; break;
+//
+//   case value2: C# / html code here; break;
+//
+//   default: C# / html code here; break;
+//
+// }
+
+namespace ViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("home")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
