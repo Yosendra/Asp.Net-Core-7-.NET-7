1574bcee08c4291313d67f9302fa01739bcfcebf
Yosendra <yosendra.evri@gmail.com>
2023-12-18 18:08:56 +0800

    Razor Views - 09. Local Functions

---
+++ 07. Razor Views/09. Local Functions/ViewExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Razor - Local Function
+//
+// @{
+//      return_type function_name(parameters) {
+//      
+//          C# / html code here
+//          
+//      } 
+// }
+//
+// Local function are callable only within the same view
+
+
+namespace ViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("home")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
