7f069b6d75fc35d62e16140c17a6c1515c3adc8e
Yosendra <yosendra.evri@gmail.com>
2023-12-18 18:56:06 +0800

    Razor Views - 10. Html.Raw

---
+++ 07. Razor Views/10. Html.Raw/ViewExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// Case : by default, asp.net will sanitize any script in variable before it get printed in view.
+//        If you don't want it, you can use 'Html.Raw()'
+// 
+// Html.Raw()
+//
+// @{
+//     string variable = "html code"
+// }
+//
+// @Html.Raw(variable)  // prints the html markup without encoding (converting html tags into plain text)
+
+namespace ViewExample.Controllers;
+
+public class HomeController : Controller
+{
+    [Route("home")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
