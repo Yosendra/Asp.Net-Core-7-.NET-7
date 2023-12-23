091c285430e19a5f5e663a43d2da85aa243ddf23
Yosendra <yosendra.evri@gmail.com>
2023-12-24 00:19:44 +0800

    Dependency Injection - 02. Services: Part 2

---
+++ 11. Dependency Injection/02. Services - Part 2/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace DIExample.Controllers;
+
+public class HomeController : Controller
+{
+    // These are the services
+    private readonly CitiesService _citiesService;
+
+    public HomeController()
+    {
+        // create object of CitiesService class
+        _citiesService = new CitiesService();   // this one is a bad practice. We have to use 'Dependency Injection'
+    }
+
+    [Route("/")]
+    public IActionResult Index()
+    {
+        // The logic how to get the data is in the Service class
+        // Controller just invoke the service and then get the data
+        List<string> cities = _citiesService.GetCities();
+
+        return View(cities);
+    }
+
+
+}
