720ed648146e968d16becdd78fde60aa376d0911
Yosendra <yosendra.evri@gmail.com>
2023-12-24 17:13:19 +0800

    Dependency Injection - 13. Autofac

---
+++ 11. Dependency Injection/13. Autofac/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace DIExample.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ICitiesService _citiesService1;
+    private readonly ICitiesService _citiesService2;
+    private readonly ICitiesService _citiesService3;
+    //private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly ILifetimeScope _lifeTimeScope;
+
+    public HomeController(ICitiesService citiesService1,
+                          ICitiesService citiesService2,
+                          ICitiesService citiesService3,
+                          //IServiceScopeFactory serviceScopeFactory)   
+                          ILifetimeScope serviceScopeFactory)           // now we are using Autofac
+    {
+        _citiesService1 = citiesService1;
+        _citiesService2 = citiesService2;
+        _citiesService3 = citiesService3;
+        //_serviceScopeFactory = serviceScopeFactory;
+        _lifeTimeScope = serviceScopeFactory;                           // now we are using Autofac
+    }
+
+    [Route("/")]
+    public IActionResult Index()
+    {
+        List<string> cities = _citiesService1.GetCities();
+
+        ViewBag.InstanceId_CitiesService_1 = _citiesService1.ServiceInstanceId;
+        ViewBag.InstanceId_CitiesService_2 = _citiesService2.ServiceInstanceId;
+        ViewBag.InstanceId_CitiesService_3 = _citiesService3.ServiceInstanceId;
+
+        //using (IServiceScope scope = _serviceScopeFactory.CreateScope())
+        using (ILifetimeScope scope = _lifeTimeScope.BeginLifetimeScope())      // now we are using Autofac
+        {
+            //ICitiesService citiesService = scope.ServiceProvider.GetRequiredService<ICitiesService>();
+            ICitiesService citiesService = scope.Resolve<ICitiesService>();     // now we are using Autofac
+            
+            ViewBag.InstanceId_CitiesService_InScope = citiesService.ServiceInstanceId;
+        }
+
+        return View(cities);
+    }
+}
---
+++ 11. Dependency Injection/13. Autofac/DIExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using ServiceContracts;
+using Services;
+
+// Autofac
+//   is another IoC container library for .Net Core
+//   Means, both are tightly-coupled.
+//
+//   Microsoft.Extensions.DependencyInjection [vs] Autofac
+//   https://autofac.readthedocs.io/en/latest/getting-started/index.html
+//
+//   
+
+var builder = WebApplication.CreateBuilder(args);
+
+// This will change the default IoC container into Autofac IoC container
+// Enable the Autofac factory
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddScoped<ICitiesService, CitiesService>();          // Register service to built-in IoC container
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>   // Register service to Autofac IoC container
+{
+    containerBuilder.RegisterType<CitiesService>().As<ICitiesService>().InstancePerDependency();      // transient
+    containerBuilder.RegisterType<CitiesService>().As<ICitiesService>().SingleInstance();             // singleton
+    containerBuilder.RegisterType<CitiesService>().As<ICitiesService>().InstancePerLifetimeScope();   // scoped
+});
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllers();
+
+app.Run();
