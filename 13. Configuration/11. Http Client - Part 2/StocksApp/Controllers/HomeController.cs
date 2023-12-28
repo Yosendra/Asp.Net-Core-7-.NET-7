138ba6573bba1db8a0ba8d5c26ce0c03fb1e7289
Yosendra <yosendra.evri@gmail.com>
2023-12-28 14:43:58 +0800

    Configuration - 11. Http Client: Part 2

---
+++ 13. Configuration/11. Http Client - Part 2/StocksApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StocksApp.Services;
+
+namespace StocksApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly MyService _myService;
+
+    public HomeController(MyService myService)
+    {
+        _myService = myService;
+    }
+
+    [Route("/")]
+    public async Task<IActionResult> Index()
+    {
+        await _myService.method();
+        return View();
+    }
+}
---
+++ 13. Configuration/11. Http Client - Part 2/StocksApp/Services/MyService.cs
@@ -0,0 +1,31 @@
+﻿namespace StocksApp.Services;
+
+public class MyService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    // Inject HttpClientFactory object in constructor
+    public MyService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task method()
+    {
+        // create an instance of HttpClient, automatically dispose
+        using HttpClient httpClient = _httpClientFactory.CreateClient();
+
+        HttpRequestMessage httpRequestMessage = new()
+        {
+            // look at https://finnhub.io/docs/api/quote documentation
+            RequestUri = new Uri("https://finnhub.io/api/v1/quote?symbol=AAPL&token=cm5shnpr01qjc6l4nqr0cm5shnpr01qjc6l4nqrg"), // this is the webservice url
+            Method = HttpMethod.Get,
+        };
+        HttpResponseMessage responseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+        Stream stream = await responseMessage.Content.ReadAsStreamAsync();
+        using StreamReader streamReader = new(stream);
+
+        string content = await streamReader.ReadToEndAsync();
+    }
+}
