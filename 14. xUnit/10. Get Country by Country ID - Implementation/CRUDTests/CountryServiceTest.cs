8c136d6c305db69c34c434d3ec40943d8df4b86b
Yosendra <yosendra.evri@gmail.com>
2023-12-30 19:59:05 +0800

    xUnit - 10. Get Country by Country ID: Implementation

---
+++ 14. xUnit/10. Get Country by Country ID - Implementation/CRUDTests/CountryServiceTest.cs
@@ -0,0 +1,165 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace CRUDTests;
+
+public class CountryServiceTest
+{
+    #region Fields
+    private readonly ICountryService _countryService;
+    #endregion
+
+    #region Constructor
+    public CountryServiceTest()
+    {
+        _countryService = new CountryService();
+    }
+    #endregion
+
+    #region AddCountry()
+    [Fact]
+    public void AddCountry_RequestModelNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        CountryAddRequest request = null;
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => 
+        {
+            // Act
+            _countryService.AddCountry(request);
+        });
+    }
+
+    [Fact]
+    public void AddCountry_CountryNameNull_ThrowsArgumentException()
+    {
+        // Arrange
+        CountryAddRequest request = new() { CountryName = null };
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => 
+        {
+            // Act
+            _countryService.AddCountry(request);
+        });
+    }
+
+    [Fact]
+    public void AddCountry_CountryNameDuplicate_ThrowsArgumentException()
+    {
+        // Arrange
+        string countryName = "USA";
+        CountryAddRequest request = new() { CountryName = countryName, };
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => 
+        {
+            // Act
+            _countryService.AddCountry(request);
+            _countryService.AddCountry(request);
+        });
+    }
+
+    [Fact]
+    public void AddCountry_Success_ReturnCountryResponseWithId()
+    {
+        // Arrange
+        string countryName = "Japan";
+        CountryAddRequest request = new() { CountryName = countryName, };
+
+        // Act
+        CountryResponse response = _countryService.AddCountry(request);
+
+        // Assert
+        Assert.True(response.CountryId != Guid.Empty);
+    }
+    #endregion
+
+    #region GetAllCountries()
+    [Fact]
+    public void GetAllCountries_EmptyList()
+    {
+        // Act
+        List<CountryResponse> actualResult = _countryService.GetAllCountries();
+
+        // Assert
+        Assert.Empty(actualResult);
+    }
+
+    [Fact]
+    public void GetAllCountries_AddFewCountries()
+    {
+        // Arrange
+        List<CountryAddRequest> countryAddRequestList = new()
+        {
+            new CountryAddRequest { CountryName = "America" },
+            new CountryAddRequest { CountryName = "Japan" },
+            new CountryAddRequest { CountryName = "Rusia" },
+        };
+
+        List<CountryResponse> expectedCountryResponseList = new();
+        foreach (var countryAddRequest in countryAddRequestList)
+        {
+            CountryResponse countryResponse = _countryService.AddCountry(countryAddRequest);
+            expectedCountryResponseList.Add(countryResponse);
+        }
+
+        // Act
+        List<CountryResponse> actualCountryResponseList = _countryService.GetAllCountries();
+
+        // Assert
+        foreach (var actualCountryResponse in actualCountryResponseList)
+        {
+            Assert.Contains(actualCountryResponse, expectedCountryResponseList);
+        }
+    }
+    #endregion
+
+    #region GetCountryById()
+    [Fact]
+    public void GetCountryById_IdNull_ReturnNull()
+    {
+        // Arrange
+        Guid? id = null;
+        
+        // Act
+        CountryResponse? result = _countryService.GetCountryById(id);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetCountryById_Exist_ReturnCountryResponse()
+    {
+        // Arrange
+        CountryAddRequest countryAddRequest = new() { CountryName = "China" };
+        CountryResponse savedCountry = _countryService.AddCountry(countryAddRequest);
+        Guid? savedCountryId = savedCountry.CountryId;
+
+        // Act
+        CountryResponse? result = _countryService.GetCountryById(savedCountryId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(savedCountry, result);
+    }
+
+    [Fact]
+    public void GetCountryById_NotExist_ReturnNull()
+    {
+        // Arrange
+        CountryAddRequest countryAddRequest = new() { CountryName = "China" };
+        _countryService.AddCountry(countryAddRequest);
+        Guid unknownId = Guid.Parse("355558CB-285F-4272-AA72-F5ECFF18DD88");
+
+        // Act
+        CountryResponse? result = _countryService.GetCountryById(unknownId);
+
+        // Assert
+        Assert.Null(result);
+    }
+    #endregion
+}
---
+++ 14. xUnit/10. Get Country by Country ID - Implementation/Services/CountryService.cs
@@ -0,0 +1,59 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services;
+
+public class CountryService : ICountryService
+{
+    private readonly List<Country> _countries;
+
+    public CountryService()
+    {
+        _countries = new List<Country>();
+    }
+
+    public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
+    {
+        ArgumentNullException.ThrowIfNull(countryAddRequest, nameof(countryAddRequest));
+
+        if (countryAddRequest.CountryName == null)
+        {
+            string errorMessage = string.Format("{0} cannot be null.", nameof(countryAddRequest.CountryName));
+            throw new ArgumentException(errorMessage);
+        }
+
+        if (_countries.Any(c => c.Name!.ToLower() == countryAddRequest.CountryName.ToLower()))
+        {
+            string errorMessage = string.Format("{0} country is already exist.", countryAddRequest.CountryName);
+            throw new ArgumentException(errorMessage);
+        }
+
+        Country country = countryAddRequest.ToCountry();
+        country.Id = Guid.NewGuid();
+        _countries.Add(country);
+
+        return country.ToCountryResponse();
+    }
+
+    public List<CountryResponse> GetAllCountries()
+    {
+        return _countries.Select(country => country.ToCountryResponse())
+                         .ToList();
+    }
+
+    public CountryResponse? GetCountryById(Guid? id)
+    {
+        // Check if Id is not null
+        if (id == null)
+            return null;
+
+        // Get matching country from List<Country> based on Id
+        Country? country = _countries.FirstOrDefault(c => c.Id == id);
+
+        // Convert matching country object to CountryResponse object
+        // Return CountryResponse object
+        // Return null if country not found
+        return country?.ToCountryResponse();
+    }
+}
