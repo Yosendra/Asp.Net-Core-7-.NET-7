1d73af1ad165dbff177d503857d7ff0dd9474f02
Yosendra <yosendra.evri@gmail.com>
2023-12-31 23:11:51 +0800

    xUnit - 14. Add Person: Implementation

---
+++ 14. xUnit/14. Add Person - Implementation/CRUDTests/PersonServiceTest.cs
@@ -0,0 +1,75 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Services;
+
+namespace CRUDTests;
+
+public class PersonServiceTest
+{
+    #region Fields
+    private readonly IPersonService _personService;
+    #endregion
+
+    #region Constructor
+    public PersonServiceTest()
+    {
+        _personService = new PersonService();
+    }
+    #endregion
+
+    #region AddPerson()
+    [Fact]
+    public void AddPerson_RequestModelNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        PersonAddRequest? requestModel = null;
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            // Act
+            _personService.AddPerson(requestModel);
+        });
+    }
+
+    [Fact]
+    public void AddPerson_NameNull_ThrowsArgumentException()
+    {
+        // Arrange
+        PersonAddRequest? requestModel = new()
+        {
+            Name = null,
+        };
+
+        // Assert
+        Assert.Throws<ArgumentException>(() =>
+        {
+            // Act
+            _personService.AddPerson(requestModel);
+        });
+    }
+
+    [Fact]
+    public void AddPerson_Success_ReturnPersonResponseWithId()
+    {
+        // Arrange
+        PersonAddRequest? requestModel = new()
+        {
+            Name = "Jack",
+            Address = "21st Jump Street",
+            Email = "Jack@mailinator.com",
+            Gender = GenderOption.Male,
+            DateOfBirth = DateTime.Parse("1995-01-01"),
+            ReceiveNewsLetters = false,
+            CountryId = Guid.NewGuid(),
+        };
+
+        // Act
+        PersonResponse response = _personService.AddPerson(requestModel);
+
+        // Assert
+        Assert.True(response.Id != Guid.Empty);
+    }
+    #endregion
+}
---
+++ 14. xUnit/14. Add Person - Implementation/Services/PersonService.cs
@@ -0,0 +1,71 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services;
+
+public class PersonService : IPersonService
+{
+    // Data Store
+    private readonly List<Person> _personDataStore;
+
+    // Service
+    private readonly ICountryService _countryService;
+
+    public PersonService()
+    {
+        _personDataStore = new();
+        _countryService = new CountryService();
+    }
+
+    public PersonResponse AddPerson(PersonAddRequest? requestModel)
+    {
+        // Check if requestModel is not null
+        ArgumentNullException.ThrowIfNull(requestModel);
+
+        // Validate all properties of requestModel
+        if (string.IsNullOrWhiteSpace(requestModel.Name))
+        {
+            string errorMessage = string.Format("{0} cannot be blank", nameof(requestModel.Name));
+            throw new ArgumentException(errorMessage);
+        }
+
+        // Convert requestModel to Person entity object
+        Person person = requestModel.ToPerson();
+
+        // Generate new id for entity object
+        person.Id = Guid.NewGuid();
+
+        // Save the entity to the data store
+        _personDataStore.Add(person);
+
+        // Return entity as PersonResponse object along with its Id
+        // case: PersonResponse contains CountryName, since we only have CountryId in person, how do we get CountryName here?
+        // Use CountryService
+
+        //var personResponse = person.ToPersonResponse();
+
+        //Guid countryId = personResponse.CountryId;
+        //CountryResponse? countryResponse = _countryService.GetCountryById(countryId);
+        //personResponse.CountryName = countryResponse?.Name;
+
+        //personResponse.CountryName = _countryService.GetCountryById(personResponse.CountryId)?.Name;
+
+        return ConvertPersonToPersonResponse(person);
+    }
+
+    public List<PersonResponse> GetAllPerson()
+    {
+        throw new NotImplementedException();
+    }
+
+    #region Private Methods
+    // Refactor to follow Single Responsibility Principle and also Reusability sake
+    private PersonResponse ConvertPersonToPersonResponse(Person person)
+    {
+        var personResponse = person.ToPersonResponse();
+        personResponse.CountryName = _countryService.GetCountryById(personResponse.CountryId)?.Name;
+        return personResponse;
+    }
+    #endregion
+}
