28a2b907a2e358d84e76b2355e3e75e15cb6eb2b
Yosendra <yosendra.evri@gmail.com>
2023-12-31 21:15:01 +0800

    xUnit - 12. Add Person - Creating Models: Part 2

---
+++ 14. xUnit/12. Add Person - Creating Models - Part 2/ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using ServiceContracts.Enums;
+
+namespace ServiceContracts.DTO;
+
+/// <summary>
+/// Acts as a DTO for inserting a new person
+/// </summary>
+public class PersonAddRequest
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public GenderOption? Gender { get; set; }
+    public string? Address { get; set; }
+    public bool ReceiveNewsLetters { get; set; }
+    public Guid? CountryId { get; set; }
+
+    /// <summary>
+    /// Convert the current object of PersonAddRequest into a new object of Person type
+    /// </summary>
+    /// <returns></returns>
+    public Person ToPerson()
+    {
+        return new()
+        {
+             Name = Name,
+             Email = Email,
+             DateOfBirth = DateOfBirth,
+             Gender = Gender.ToString(),
+             Address = Address,
+             ReceiveNewsLetters = ReceiveNewsLetters,
+             CountryId = CountryId,
+        };
+    }
+}
---
+++ 14. xUnit/12. Add Person - Creating Models - Part 2/Services/CountryService.cs
@@ -0,0 +1,53 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services;
+
+public class CountryService : ICountryService
+{
+    private readonly List<Country> _countries;
+
+    public CountryService()
+    {
+        _countries = new List<Country>();
+    }
+
+    public CountryResponse AddCountry(CountryAddRequest? requestModel)
+    {
+        ArgumentNullException.ThrowIfNull(requestModel, nameof(requestModel));
+
+        if (requestModel.Name == null)
+        {
+            string errorMessage = string.Format("{0} cannot be null.", nameof(requestModel.Name));
+            throw new ArgumentException(errorMessage);
+        }
+
+        requestModel.Name = requestModel.Name.Trim();
+        if (_countries.Any(c => c.Name!.ToLower() == requestModel.Name.ToLower()))
+        {
+            string errorMessage = string.Format("{0} country is already exist.", requestModel.Name);
+            throw new ArgumentException(errorMessage);
+        }
+
+        Country country = requestModel.ToCountry();
+        country.Id = Guid.NewGuid();
+        _countries.Add(country);
+
+        return country.ToCountryResponse();
+    }
+
+    public List<CountryResponse> GetAllCountries()
+    {
+        return _countries.Select(country => country.ToCountryResponse()).ToList();
+    }
+
+    public CountryResponse? GetCountryById(Guid? id)
+    {
+        if (id == null) return null;
+
+        Country? country = _countries.FirstOrDefault(c => c.Id == id);
+
+        return country?.ToCountryResponse();
+    }
+}
