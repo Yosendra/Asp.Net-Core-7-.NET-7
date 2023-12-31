402b9742be04c46d6e32266d4f777adac6aa1585
Yosendra <yosendra.evri@gmail.com>
2024-01-01 03:41:53 +0800

    xUnit - 17. Get Person By Person ID: Implementation

---
+++ 14. xUnit/17. Get Person By Person ID - Implementation/CRUDTests/PersonServiceTest.cs
@@ -0,0 +1,119 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Services;
+
+namespace CRUDTests;
+
+public class PersonServiceTest
+{
+    #region Fields
+    private readonly IPersonService _personService;
+    private readonly ICountryService _countryService;
+    #endregion
+
+    #region Constructor
+    public PersonServiceTest()
+    {
+        _personService = new PersonService();
+        _countryService = new CountryService();
+    }
+    #endregion
+
+    #region AddPerson()
+    [Fact]
+    public void AddPerson_RequestModelNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        PersonAddRequest? requestModel = null;
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            // Act
+            _personService.AddPerson(requestModel);
+        });
+    }
+
+    [Fact]
+    public void AddPerson_NameNull_ThrowsArgumentException()
+    {
+        // Arrange
+        PersonAddRequest? requestModel = new()
+        {
+            Name = null,
+        };
+
+        // Assert
+        Assert.Throws<ArgumentException>(() =>
+        {
+            // Act
+            _personService.AddPerson(requestModel);
+        });
+    }
+
+    [Fact]
+    public void AddPerson_Success_ReturnPersonResponseWithId()
+    {
+        // Arrange
+        PersonAddRequest? requestModel = new()
+        {
+            Name = "Jack",
+            Address = "21st Jump Street",
+            Email = "Jack@mailinator.com",
+            Gender = GenderOption.Male,
+            DateOfBirth = DateTime.Parse("1995-01-01"),
+            ReceiveNewsLetters = false,
+            CountryId = Guid.NewGuid(),
+        };
+
+        // Act
+        PersonResponse response = _personService.AddPerson(requestModel);
+
+        // Assert
+        Assert.True(response.Id != Guid.Empty);
+    }
+    #endregion
+
+    #region GetPersonById
+    [Fact]
+    public void GetPersonById_IdNull_ReturnNull()
+    {
+        // Arrange
+        Guid? id = null;
+
+        // Act
+        PersonResponse? response = _personService.GetPersonById(id);
+
+        // Assert
+        Assert.Null(response);
+    }
+
+    [Fact]
+    public void GetPersonById_Exist_ReturnCorrectPersonResponse()
+    {
+        // Arrange
+        CountryAddRequest countryAddRequest = new(){ Name = "Canada", };
+        CountryResponse countryResponse = _countryService.AddCountry(countryAddRequest);
+
+        PersonAddRequest? personAddRequest = new()
+        {
+            Name = "Jack",
+            Address = "21st Jump Street",
+            Email = "Jack@mailinator.com",
+            Gender = GenderOption.Male,
+            DateOfBirth = DateTime.Parse("1995-01-01"),
+            ReceiveNewsLetters = false,
+            CountryId = countryResponse.Id,
+        };
+        PersonResponse expectedPersonResponse = _personService.AddPerson(personAddRequest);
+        Guid? addedPersonId = expectedPersonResponse.Id;
+
+        // Act
+        PersonResponse? actualPersonResponse = _personService.GetPersonById(addedPersonId);
+
+        // Assert
+        Assert.Equal(expectedPersonResponse, actualPersonResponse);
+    }
+    #endregion
+}
---
+++ 14. xUnit/17. Get Person By Person ID - Implementation/Services/PersonService.cs
@@ -0,0 +1,64 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Services.Helper;
+
+namespace Services;
+
+public class PersonService : IPersonService
+{
+    // Data Store
+    private readonly List<Person> _personDataStore;
+
+    // Service
+    private readonly ICountryService _countryService;
+
+    public PersonService()
+    {
+        _personDataStore = new();
+        _countryService = new CountryService();
+    }
+
+    public PersonResponse AddPerson(PersonAddRequest? requestModel)
+    {
+        ArgumentNullException.ThrowIfNull(requestModel);
+
+        ValidationHelper.ModelValidation(requestModel);
+
+        Person person = requestModel.ToPerson();
+        person.Id = Guid.NewGuid();
+        _personDataStore.Add(person);
+
+        return ConvertPersonToPersonResponse(person);
+    }
+
+    public List<PersonResponse> GetAllPerson()
+    {
+        throw new NotImplementedException();
+    }
+
+    public PersonResponse? GetPersonById(Guid? id)
+    {
+        // Check if id is not null
+        if (id == null)
+            return null;
+
+        // Get the matching person based on id from data store
+        Person? person = _personDataStore.FirstOrDefault(p => p.Id == id);
+
+        // Convert matching person object from Person to PersonResponse type
+        // Return PersonResponse object
+        return person != null
+            ? ConvertPersonToPersonResponse(person)
+            : null;
+    }
+
+    #region Private Methods
+    private PersonResponse ConvertPersonToPersonResponse(Person person)
+    {
+        var personResponse = person.ToPersonResponse();
+        personResponse.CountryName = _countryService.GetCountryById(personResponse.CountryId)?.Name;
+        return personResponse;
+    }
+    #endregion
+}
