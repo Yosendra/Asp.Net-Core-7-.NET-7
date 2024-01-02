35f00de18db2c64bb364d980b35b9ab779b1718c
Yosendra <yosendra.evri@gmail.com>
2024-01-03 02:43:53 +0800

    CRUD Operation - 03. List View

---
+++ 15. CRUD Operation/03. List View/CRUDExample/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace CRUDExample.Controllers;
+
+public class PersonController : Controller
+{
+    private readonly IPersonService _personService;
+
+    public PersonController(IPersonService personService)
+    {
+        _personService = personService;
+    }
+
+    [Route("person/index")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        var persons = _personService.GetAllPersons();
+
+        return View(persons);
+    }
+}
