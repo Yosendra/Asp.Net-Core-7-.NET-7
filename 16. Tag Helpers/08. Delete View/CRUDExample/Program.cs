dab9af25b40c6cfaac131ab1929720eb35e5a577
Yosendra <yosendra.evri@gmail.com>
2024-01-06 22:39:25 +0800

    Tag Helpers - 08. Delete View

---
+++ 16. Tag Helpers/08. Delete View/CRUDExample/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using ServiceContracts;
+using Services;
+
+// Look at PersonController -> Delete page and process, 
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+// Add services into IoC container
+builder.Services.AddSingleton<ICountryService, CountryService>();   // notice here we use Singleton to make the
+builder.Services.AddSingleton<IPersonService, PersonService>();     // data store alive until application shutdown
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
