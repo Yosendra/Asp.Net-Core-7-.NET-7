71638f39a8c566546baf3e8fe828ab803f7d456d
Yosendra <yosendra.evri@gmail.com>
2024-01-18 00:27:32 +0800

    EntityFrameworkCore - 11. Fluent API: Part 1

---
+++ 17. Entity Framework Core/11. Fluent API - Part 1/Services/CountryService.cs
@@ -0,0 +1,54 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services;
+
+public class CountryService : ICountryService
+{
+    private readonly PersonsDbContext _db;
+
+    public CountryService(PersonsDbContext personsDbContext)
+    {
+        _db = personsDbContext;
+    }
+
+    public CountryResponse AddCountry(CountryAddRequest? requestModel)
+    {
+        ArgumentNullException.ThrowIfNull(requestModel, nameof(requestModel));
+
+        if (requestModel.Name == null)
+        {
+            string errorMessage = string.Format("{0} cannot be null.", nameof(requestModel.Name));
+            throw new ArgumentException(errorMessage);
+        }
+
+        requestModel.Name = requestModel.Name.Trim();
+        if (_db.Countries.Any(c => c.Name!.ToLower() == requestModel.Name.ToLower()))
+        {
+            string errorMessage = string.Format("{0} country is already exist.", requestModel.Name);
+            throw new ArgumentException(errorMessage);
+        }
+
+        Country country = requestModel.ToCountry();
+        country.Id = Guid.NewGuid();
+        _db.Countries.Add(country);
+        _db.SaveChanges();
+
+        return country.ToCountryResponse();
+    }
+
+    public List<CountryResponse> GetAllCountries()
+    {
+        return _db.Countries.Select(country => country.ToCountryResponse()).ToList();
+    }
+
+    public CountryResponse? GetCountryById(Guid? id)
+    {
+        if (id == null) return null;
+
+        Country? country = _db.Countries.FirstOrDefault(c => c.Id == id);
+
+        return country?.ToCountryResponse();
+    }
+}
