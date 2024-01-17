3ad6e8c86fbfa8ec7baca6ec3272267bc13a2c66
Yosendra <yosendra.evri@gmail.com>
2024-01-17 21:47:19 +0800

    EntityFrameworkCore - 09. EF Core: Stored Procedure with Parameters

---
+++ 17. Entity Framework Core/09. EF Core - Stored Procedure with Parameters/CRUDExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+/* EF Core - Stored Procedure with Parameters
+ *   If you want to perform multiple sql statement or complex database operation through a single database call
+ *   then creating stored procedure in the database is the best choice
+ *   
+ *   Calling Stored Procedure
+ *     For INSERT, UPDATE, DELETE   
+ *       int DbContext.Database.ExecuteSqlRaw(
+ *         string sql,                              // Eg: "EXECUTE [dbo].[store_procedure_name] @param1 @param2"
+ *         params object[] parameters               // A list of objects of SqlParameter type
+ *       )
+ *       
+ *     For SELECT   
+ *       IQueryable<Model> [DbSetName].FromSqlRaw(
+ *         string sql,                              // Eg: "EXECUTE [dbo].[store_procedure_name] @param1 @param2"
+ *         params object[] parameters               // A list of objects of SqlParameter type
+ *       )
+ *       
+ *   First add the SP through Migration.
+ *   
+ *   How to create Store Procedure throug code (migration)?
+ *      In Package Manager Console type: Add-Migration InsertPerson_StoredProcedure
+ *   
+ *   Create SP with Parameters
+ *     CREATE PROCEDURE [schema].[procedure_name] (@paramater_one data_type, @paramater_two data_type)
+ *     AS BEGIN
+ *       statement...
+ *     END
+ *   
+ *   Take a look at two methods generated from Add-Migration command at Migration file. There are Up() and Down()
+ *   Whenever you put command Update-Database, the Up() is executed, when you want to rollback, then the Down() method is executed. 
+ *   
+ *   Look at:
+ *     20240117130320_InsertPerson_StoredProcedure.cs, we write our SP statment inside Up() method
+ *     PersonService.cs at AddPerson() method
+ *     PersonsDbContext.cs at 
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<PersonsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
---
+++ 17. Entity Framework Core/09. EF Core - Stored Procedure with Parameters/Services/PersonService.cs
@@ -0,0 +1,189 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Services.Helper;
+
+namespace Services;
+
+public class PersonService : IPersonService
+{
+    private readonly PersonsDbContext _db;
+    private readonly ICountryService _countryService;
+
+    public PersonService(PersonsDbContext personsDbContext, ICountryService countryService)
+    {
+        _db = personsDbContext;
+        _countryService = countryService;
+    }
+
+    public PersonResponse AddPerson(PersonAddRequest? requestModel)
+    {
+        ArgumentNullException.ThrowIfNull(requestModel);
+
+        ValidationHelper.ModelValidation(requestModel);
+
+        Person person = requestModel.ToPerson();
+        person.Id = Guid.NewGuid();
+
+        //_db.Persons.Add(person);
+        //_db.SaveChanges();
+
+        // Here use our SP
+        _db.sp_InsertPerson(person);
+
+        return ConvertPersonToPersonResponse(person);
+    }
+
+    public PersonResponse? GetPersonById(Guid? id)
+    {
+        if (id == null)
+            return null;
+
+        Person? person = _db.Persons.FirstOrDefault(p => p.Id == id);
+
+        return person != null
+            ? ConvertPersonToPersonResponse(person)
+            : null;
+    }
+
+    public List<PersonResponse> GetAllPersons()
+    {
+        //return _db.Persons.ToList().
+        //    Select(p => ConvertPersonToPersonResponse(p)).ToList();
+
+        // We call the SP we defined here
+        return _db.sp_GetAllPersons().Select(p => ConvertPersonToPersonResponse(p)).ToList();
+    }
+
+    public List<PersonResponse> GetFilteredPersons(string searchBy, string? keyword)
+    {
+        List<PersonResponse> allPersons = GetAllPersons();
+        List<PersonResponse> matchingPersons = allPersons;
+
+        if (string.IsNullOrWhiteSpace(searchBy) || (!string.IsNullOrWhiteSpace(searchBy) && string.IsNullOrWhiteSpace(keyword))) 
+            return matchingPersons;
+
+        switch (searchBy)
+        {
+            case nameof(PersonResponse.Name):
+                matchingPersons = allPersons.Where(p => p.Name.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+                break;
+
+            case nameof(PersonResponse.Email):
+                matchingPersons = allPersons.Where(p => p.Email.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+                break;
+
+            case nameof(PersonResponse.DateOfBirth):
+                matchingPersons = allPersons.Where(p => p.DateOfBirth.Value
+                                                                     .ToString("dd MMMM yyyy")
+                                                                     .Contains(keyword, StringComparison.OrdinalIgnoreCase))
+                                                                     .ToList();
+                break;
+
+            case nameof(PersonResponse.Gender):
+                matchingPersons = allPersons.Where(p => p.Gender.Equals(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+                break;
+
+            case nameof(PersonResponse.CountryId):
+                matchingPersons = allPersons.Where(p => p.CountryName.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+                break;
+
+            case nameof(PersonResponse.Address):
+                matchingPersons = allPersons.Where(p => string.IsNullOrWhiteSpace(p.Address) 
+                                                        ? false
+                                                        : p.Address.Contains(keyword, StringComparison.OrdinalIgnoreCase)).ToList();
+                break;
+
+            default:
+                matchingPersons = allPersons;
+                break;
+        }
+
+        return matchingPersons;
+    }
+
+    public List<PersonResponse> GetSortedPersons(List<PersonResponse> allPersons, string sortBy, SortOrderEnum sortOrder)
+    {
+        if (string.IsNullOrWhiteSpace(sortBy))
+            return allPersons;
+
+        List<PersonResponse> sortedPersonList = (sortBy, sortOrder) switch
+        {
+            (nameof(PersonResponse.Name), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.Name, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Name), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.Name, StringComparer.OrdinalIgnoreCase).ToList(),
+
+            (nameof(PersonResponse.Email), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.Email, StringComparer.OrdinalIgnoreCase).ToList(),
+            (nameof(PersonResponse.Email), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.Email, StringComparer.OrdinalIgnoreCase).ToList(),
+
+            (nameof(PersonResponse.DateOfBirth), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.DateOfBirth).ToList(),
+            (nameof(PersonResponse.DateOfBirth), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.DateOfBirth).ToList(),
+
+            (nameof(PersonResponse.Age), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.Age).ToList(),
+            (nameof(PersonResponse.Age), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.Age).ToList(),
+
+            (nameof(PersonResponse.Gender), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.Gender).ToList(),
+            (nameof(PersonResponse.Gender), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.Gender).ToList(),
+
+            (nameof(PersonResponse.CountryName), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.CountryName).ToList(),
+            (nameof(PersonResponse.CountryName), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.CountryName).ToList(),
+
+            (nameof(PersonResponse.Address), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.Address).ToList(),
+            (nameof(PersonResponse.Address), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.Address).ToList(),
+
+            (nameof(PersonResponse.ReceiveNewsLetters), SortOrderEnum.ASC) => allPersons.OrderBy(p => p.ReceiveNewsLetters).ToList(),
+            (nameof(PersonResponse.ReceiveNewsLetters), SortOrderEnum.DESC) => allPersons.OrderByDescending(p => p.ReceiveNewsLetters).ToList(),
+
+            _ => allPersons
+        };
+
+        return sortedPersonList;
+    }
+
+    public PersonResponse UpdatePerson(PersonUpdateRequest? requestModel)
+    {
+        ArgumentNullException.ThrowIfNull(requestModel);
+
+        ValidationHelper.ModelValidation(requestModel);
+
+        Person matchingPerson = _db.Persons.FirstOrDefault(p => p.Id == requestModel.Id);
+
+        if (matchingPerson == null)
+            throw new ArgumentException("Given id doesn't exist");
+
+        matchingPerson.Name = requestModel.Name;
+        matchingPerson.Email = requestModel.Email;
+        matchingPerson.DateOfBirth = requestModel.DateOfBirth;
+        matchingPerson.Gender = requestModel.Gender.ToString();
+        matchingPerson.CountryId = requestModel.CountryId;
+        matchingPerson.Address = requestModel.Address;
+        matchingPerson.ReceiveNewsLetters = requestModel.ReceiveNewsLetters;
+        _db.SaveChanges();
+
+        return ConvertPersonToPersonResponse(matchingPerson);
+    }
+
+    public bool DeletePerson(Guid? id)
+    {
+        ArgumentNullException.ThrowIfNull(id);
+
+        Person? person = _db.Persons.FirstOrDefault(p => p.Id == id);
+        if (person == null)
+            return false;
+
+        _db.Persons.Remove(person);
+        _db.SaveChanges();
+
+        return true;
+    }
+
+    #region Private Methods
+    private PersonResponse ConvertPersonToPersonResponse(Person person)
+    {
+        var personResponse = person.ToPersonResponse();
+        personResponse.CountryName = _countryService.GetCountryById(personResponse.CountryId)?.Name;
+        return personResponse;
+    }
+
+    #endregion
+}
