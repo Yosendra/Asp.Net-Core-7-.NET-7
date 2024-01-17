815ca45e496cceb448daba7d89f98c08174aab5d
Yosendra <yosendra.evri@gmail.com>
2024-01-18 00:05:53 +0800

    EntityFrameworkCore - 10. Changes in Table Structure

---
+++ 17. Entity Framework Core/10. Changes in Table Structure/CRUDExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+/* Changes in Table Structure
+ *   case: we want to add a new column in Person table that is "TIN" or Text Identification Number
+ *   
+ *   Add new property at Person.cs: public string? TIN { get; set; }
+ *   Go to Package Manager Console, then add migration
+ *   Migration generating the new column in the model, waiting to be updated by Update-Database command
+ *   
+ *   Look at: Person.cs, we add TIN property
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<PersonsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
---
+++ 17. Entity Framework Core/10. Changes in Table Structure/ServiceContracts/Enums/GenderOption.cs
@@ -0,0 +1,6 @@
+﻿namespace ServiceContracts.Enums;
+
+public enum GenderOption
+{
+    Male, Female
+}
