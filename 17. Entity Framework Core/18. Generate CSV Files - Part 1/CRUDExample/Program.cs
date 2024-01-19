795b508b788f85c534c3e824b3260ac3c0c86a8d
Yosendra <yosendra.evri@gmail.com>
2024-01-19 22:02:55 +0800

    EntityFrameworkCore - 18. Generate CSV Files: Part 1

---
+++ 17. Entity Framework Core/18. Generate CSV Files - Part 1/CRUDExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Using 'CsvHelper' as 3rd party package to generate CSV file (Install through NuGet Package Manager)
+ * 
+ *   Look at: IPersonService we add new contract "Task<MemoryStream> GetPersonCSV()" then implement this in PersonService
+ *            Install CsvHelper package at Services project
+ *            Implement GetPersonCSV() at PersonService
+ *            Add new endpoint for converting CSV file at PersonController, in this case it is PersonCSV()
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<PersonsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
---
+++ 17. Entity Framework Core/18. Generate CSV Files - Part 1/Services/CountryService.cs
@@ -0,0 +1,55 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services;
+
+public class CountryService : ICountryService
+{
+    private readonly PersonsDbContext _db;
+
+    public CountryService(PersonsDbContext personsDbContext)
+    {
+        _db = personsDbContext;
+    }
+
+    public async Task<CountryResponse> AddCountry(CountryAddRequest? requestModel)
+    {
+        ArgumentNullException.ThrowIfNull(requestModel, nameof(requestModel));
+
+        if (requestModel.Name == null)
+        {
+            string errorMessage = string.Format("{0} cannot be null.", nameof(requestModel.Name));
+            throw new ArgumentException(errorMessage);
+        }
+
+        requestModel.Name = requestModel.Name.Trim();
+        if (await _db.Countries.AnyAsync(c => c.Name!.ToLower() == requestModel.Name.ToLower()))
+        {
+            string errorMessage = string.Format("{0} country is already exist.", requestModel.Name);
+            throw new ArgumentException(errorMessage);
+        }
+
+        Country country = requestModel.ToCountry();
+        country.Id = Guid.NewGuid();
+        _db.Countries.Add(country);
+        await _db.SaveChangesAsync();
+
+        return country.ToCountryResponse();
+    }
+
+    public async Task<List<CountryResponse>> GetAllCountries()
+    {
+        return await _db.Countries.Select(country => country.ToCountryResponse()).ToListAsync();
+    }
+
+    public async Task<CountryResponse?> GetCountryById(Guid? id)
+    {
+        if (id == null) return null;
+
+        Country? country = await _db.Countries.FirstOrDefaultAsync(c => c.Id == id);
+
+        return country?.ToCountryResponse();
+    }
+}
