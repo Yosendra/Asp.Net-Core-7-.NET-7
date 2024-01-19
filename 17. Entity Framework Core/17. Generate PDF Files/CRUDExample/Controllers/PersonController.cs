3395b4be1127fa41994e4d5f33fd01dbefdc9e7b
Yosendra <yosendra.evri@gmail.com>
2024-01-19 21:22:55 +0800

    EntityFrameworkCore - 17. Generate PDF Files

---
+++ 17. Entity Framework Core/17. Generate PDF Files/CRUDExample/Controllers/PersonController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Rotativa.AspNetCore;
+using Rotativa.AspNetCore.Options;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+
+namespace CRUDExample.Controllers;
+
+[Route("[controller]")]
+public class PersonController : Controller
+{
+    private readonly IPersonService _personService;
+    private readonly ICountryService _countryService;
+
+    public PersonController(IPersonService personService, ICountryService countryService)
+    {
+        _personService = personService;
+        _countryService = countryService;
+    }
+
+    [Route("/")]
+    [Route("[action]")]
+    public async Task<IActionResult> Index(
+        string searchBy,
+        string? keyword,
+        string sortBy = nameof(PersonResponse.Name),
+        SortOrderEnum sortOrder = SortOrderEnum.ASC)
+    {
+        var persons = await _personService.GetFilteredPersons(searchBy, keyword);
+        persons = await _personService.GetSortedPersons(persons, sortBy, sortOrder);
+
+        ViewBag.SearchFields = new Dictionary<string, string>()
+        {
+            {nameof(PersonResponse.Name), "Name"},
+            {nameof(PersonResponse.Email), "Email"},
+            {nameof(PersonResponse.DateOfBirth), "Date of Birth"},
+            {nameof(PersonResponse.Gender), "Gender"},
+            {nameof(PersonResponse.CountryId), "Country"},
+            {nameof(PersonResponse.Address), "Address"},
+        };
+        ViewBag.CurrentSearchBy = searchBy;
+        ViewBag.CurrentKeyword = keyword;
+        ViewBag.CurrentSortBy = sortBy;
+        ViewBag.CurrentSortOrder = sortOrder.ToString();
+
+        return View(persons);
+    }
+
+    [HttpGet]
+    [Route("[action]")]
+    public async Task<IActionResult> Create()
+    {
+        var countries = await _countryService.GetAllCountries();
+        ViewBag.Countries = countries.Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+
+        return View();
+    }
+
+    [HttpPost]
+    [Route("[action]")]
+    public async Task<IActionResult> Create(PersonAddRequest requestModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            List<CountryResponse> countries = await _countryService.GetAllCountries();
+            ViewBag.Countries = countries.Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+            ViewBag.Errors = ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage)
+                .ToList();
+
+            return View();
+        }
+
+        PersonResponse personAdded = await _personService.AddPerson(requestModel);
+
+        return RedirectToAction("Index", "Person");
+    }
+
+    [HttpGet]
+    [Route("[action]/{id}")]
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        PersonResponse? person = await _personService.GetPersonById(id);
+        if (person == null)
+            return RedirectToAction("Index");
+
+        var countries = await _countryService.GetAllCountries();
+        ViewBag.Countries = countries.Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+
+        PersonUpdateRequest model = person.ToPersonUpdateRequest();
+        return View(model);
+    }
+
+    [HttpPost]
+    [Route("[action]/{id}")]
+    public async Task<IActionResult> Edit(PersonUpdateRequest updatePersonModel)
+    {
+        PersonResponse? person = await _personService.GetPersonById(updatePersonModel.Id);
+        if (person == null)
+            return RedirectToAction("Index");
+
+        if (ModelState.IsValid)
+        {
+            await _personService.UpdatePerson(updatePersonModel);
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            List<CountryResponse> countries = await _countryService.GetAllCountries();
+            ViewBag.Countries = countries.Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+            ViewBag.Errors = ModelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage)
+                .ToList();
+            
+            return View(person.ToPersonUpdateRequest());
+        }
+    }
+
+    [HttpGet]
+    [Route("[action]/{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        PersonResponse? person = await _personService.GetPersonById(id);
+        if (person == null)
+            return RedirectToAction("Index");
+
+        return View(person);
+    }
+
+    [HttpPost]
+    [Route("[action]/{id}")]
+    public async Task<IActionResult> Delete(PersonUpdateRequest updatePersonModel)
+    {
+        PersonResponse? person = await _personService.GetPersonById(updatePersonModel.Id);
+        if (person == null)
+            return RedirectToAction("Index");
+
+        await _personService.DeletePerson(person.Id);
+        
+        return RedirectToAction("Index");
+    }
+
+    [Route("[action]")]
+    public async Task<IActionResult> PersonPDF()
+    {
+        // Get all person
+        var persons = await _personService.GetAllPersons();
+
+        // Return view as PDF
+        return new ViewAsPdf(persons)
+        {
+            PageMargins = new Margins()
+            {
+                Top = 20, Right = 20, Bottom = 20, Left = 20,
+            },
+            PageOrientation = Orientation.Landscape,
+        };
+    }
+}
---
+++ 17. Entity Framework Core/17. Generate PDF Files/CRUDExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Using 'Rotativa' as 3rd party package to generate PDF file (Install through NuGet Package Manager)
+ * 
+ *   Look at: PersonPDF.cshtml as the base view of our PDF page
+ *            PersonController add action for generating the PDF, in this case it is PersonPDF() action in PersonController
+ *            In order to Rotativa able to work, we need exe file wkhtmltopdf.exe placed in wwwroot folder
+ *            Configure the wkhtmltopdf.exe path for Rotativa
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<PersonsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot"); // notice this
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
