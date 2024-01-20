1e6d05fafa9a44ee48d0ee391f867bf7fa32e8ec
Yosendra <yosendra.evri@gmail.com>
2024-01-20 18:48:21 +0800

    EntityFrameworkCore - 20. Generate Excel Files

---
+++ 17. Entity Framework Core/20. Generate Excel Files/CRUDExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Generate Excel Files
+ *   Install EPPlus package at Service project
+ *   App some key in appsettings.json for non-commercial use
+ *   Add new method in PersonService contract and implementation -> GetPersonExcel()
+ *   Add new endpoint in PersonController -> PersonExcel()
+ * 
+ *   Look at: PersonService, PersonController, Index page for Person (adding hyperlink to download excel file)
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<PersonsDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
---
+++ 17. Entity Framework Core/20. Generate Excel Files/Entities/PersonsDbContext.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities;
+
+public class PersonsDbContext : DbContext
+{
+    public DbSet<Country> Countries { get; set;}
+    public DbSet<Person> Persons { get; set;}
+
+    public PersonsDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Country>().ToTable("Countries");
+        modelBuilder.Entity<Person>().ToTable("Persons");
+
+        // Seed to Countries
+        string countriesJson = File.ReadAllText("countries.json");
+        var listOfCountries = JsonSerializer.Deserialize<List<Country>>(countriesJson);
+        foreach (var country in listOfCountries)
+            modelBuilder.Entity<Country>().HasData(country);
+
+        // Seed to Persons
+        string personsJson = File.ReadAllText("persons.json");
+        var listOfPersons = JsonSerializer.Deserialize<List<Person>>(personsJson);
+        foreach (var person in listOfPersons)
+            modelBuilder.Entity<Person>().HasData(person);
+
+        // Fluent API
+        modelBuilder.Entity<Person>().Property(c => c.TIN)
+            .HasColumnName("TaxIdentificationNumber")
+            .HasColumnType("varchar(8)")
+            .HasDefaultValue("ABC12345");
+
+        modelBuilder.Entity<Person>()
+            .HasCheckConstraint("CHK_TIN", "len([TaxIdentificationNumber]) = 8");
+    }
+
+    #region Stored Procedure
+    public async Task<List<Person>> sp_GetAllPersons()
+        => await Persons.FromSqlRaw("EXECUTE [dbo].[GetAllPersons]").ToListAsync();
+
+    public async Task<int> sp_InsertPerson(Person person)
+    {
+        SqlParameter[] parameters =
+        {
+            new("@Id", person.Id),
+            new("@Name", person.Name),
+            new("@Email", person.Email),
+            new("@DateOfBirth", person.DateOfBirth),
+            new("@Gender", person.Gender),
+            new("@Address", person.Address),
+            new("@ReceiveNewsLetters", person.ReceiveNewsLetters),
+            new("@CountryId", person.CountryId),
+        };
+
+        return await Database.ExecuteSqlRawAsync("EXECUTE [dbo].[InsertPerson] " +
+            "@Id, @Name, @Email, @DateOfBirth, @Gender, @Address, @ReceiveNewsLetters, @CountryId",
+            parameters);
+    }
+    #endregion
+}
