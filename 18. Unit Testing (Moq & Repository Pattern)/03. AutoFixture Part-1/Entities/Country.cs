50f5494e59eaf2b69447f8a1ecfee0ef6ad1da80
Yosendra <yosendra.evri@gmail.com>
2024-01-21 16:51:28 +0800

    Unit Testing - 03. AutoFixture Part: 1

---
+++ 18. Unit Testing (Moq & Repository Pattern)/03. AutoFixture Part-1/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+
+namespace Entities;
+
+/// <summary>
+/// Domain Model for Country
+/// </summary>
+public class Country
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+
+    public virtual ICollection<Person>? Persons { get; set; }
+}
