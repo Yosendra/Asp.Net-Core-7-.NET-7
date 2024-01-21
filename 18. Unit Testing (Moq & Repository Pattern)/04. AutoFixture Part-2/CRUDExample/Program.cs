7a8eb887a620e67a122ebbced31f53d177068f2a
Yosendra <yosendra.evri@gmail.com>
2024-01-21 17:51:43 +0800

    Unit Testing - 04. AutoFixture Part: 2

---
+++ 18. Unit Testing (Moq & Repository Pattern)/04. AutoFixture Part-2/CRUDExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Auto Fixture
+ *   generates objects of the specified classes and their properties with some fake values based their data type.
+ *     
+ *     AutoFixture               AutoFixture
+ *       Create object    --->     Fill dummy values into properties
+ *       
+ *     Normal object creation    With AutoFixture
+ *       new ModelClass() {      Fixture.Create<ModelClass>();
+ *         Porperty1 = value,    // Initializes all properties of the specified model class with dummy values
+ *         Property2 = value,
+ *       }
+ *       
+ *   Install AutoFixture package at ServiceTest project 
+ *   Use the AutoFixture at the service test file
+ *   We add private field for fixture and initialize it in the contructor
+ *   
+ *   Look at: PersonServiceTest at AddPerson_Success_ReturnPersonResponseWithId()
+ *   
+ *   Now we implement AutoFixture for all test case method in PersonServiceTest
+ *   
+ *   CountryServiceTest still not implemented
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
---
+++ 18. Unit Testing (Moq & Repository Pattern)/04. AutoFixture Part-2/ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,46 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO;
+
+/// <summary>
+/// DTO class that is used as return type for most of CountryService methods
+/// </summary>
+public class CountryResponse
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+
+    #region Override Method
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (obj.GetType() != typeof(CountryResponse))
+            return false;
+
+        var target = (CountryResponse)obj;
+        return this.Id == target.Id
+                && this.Name == target.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+    #endregion
+}
+
+// Extension method
+public static class CountryExtension 
+{
+    public static CountryResponse ToCountryResponse(this Country country)
+    {
+        return new()
+        {
+            Id = country.Id,
+            Name = country.Name,
+        };
+    }
+}
+
