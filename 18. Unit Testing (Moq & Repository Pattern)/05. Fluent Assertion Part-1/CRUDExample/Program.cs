fb4e22372b7d6cf12a74018a6ecb8057db89d599
Yosendra <yosendra.evri@gmail.com>
2024-01-21 18:54:42 +0800

    Unit Testing - 05. Fluent Assertion Part: 1

---
+++ 18. Unit Testing (Moq & Repository Pattern)/05. Fluent Assertion Part-1/CRUDExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Fluent Assertion
+ *   Fluent Assertion are a set of extension method to make the assertions in unit testing readable and human-friendly
+ *   
+ *   Install-Package FluentAssertions
+ *   
+ *   Assert                                 Fluent Assertion
+ *     Assert.Equal(expected, actual)         actual.Should().Be(expected)
+ *   
+ *   Look at: PersonServiceTest.cs
+ *   
+ *   Not Completed Yet
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
---
+++ 18. Unit Testing (Moq & Repository Pattern)/05. Fluent Assertion Part-1/CRUDTests/CountryServiceTest.cs
@@ -0,0 +1,177 @@
+﻿using Entities;
+using EntityFrameworkCoreMock;
+using Microsoft.EntityFrameworkCore;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace CRUDTests;
+
+public class CountryServiceTest
+{
+    #region Fields
+    private readonly ICountryService _countryService;
+    #endregion
+
+    #region Constructor
+    public CountryServiceTest()
+    {
+        var initialData = new List<Country>();
+
+        // Mock the DbContext
+        var dbContextOption = new DbContextOptionsBuilder<ApplicationDbContext>().Options;
+        DbContextMock<ApplicationDbContext> dbContextMock = new(dbContextOption);
+
+        // Mock the DbSet functionality and load the initial data for countries
+        dbContextMock.CreateDbSetMock(t => t.Countries, initialData);
+
+        ApplicationDbContext dbContext = dbContextMock.Object;
+
+        _countryService = new CountryService(dbContext);
+    }
+    #endregion
+
+    #region AddCountry()
+    [Fact]
+    public async Task AddCountry_RequestModelNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        CountryAddRequest request = null;
+
+        // Assert
+        await Assert.ThrowsAsync<ArgumentNullException>(async () =>         // notice this
+        {
+            // Act
+            await _countryService.AddCountry(request);
+        });
+    }
+
+    [Fact]
+    public async Task AddCountry_CountryNameNull_ThrowsArgumentException()
+    {
+        // Arrange
+        CountryAddRequest request = new() { Name = null };
+
+        // Assert
+        await Assert.ThrowsAsync<ArgumentException>(async () => 
+        {
+            // Act
+            await _countryService.AddCountry(request);
+        });
+    }
+
+    [Fact]
+    public async Task AddCountry_CountryNameDuplicate_ThrowsArgumentException()
+    {
+        // Arrange
+        string countryName = "USA";
+        CountryAddRequest request = new() { Name = countryName, };
+
+        // Assert
+        await Assert.ThrowsAsync<ArgumentException>(async () => 
+        {
+            // Act
+            await _countryService.AddCountry(request);
+            await _countryService.AddCountry(request);
+        });
+    }
+
+    [Fact]
+    public async Task AddCountry_Success_ReturnCountryResponseWithId()
+    {
+        // Arrange
+        string countryName = "Japan";
+        CountryAddRequest request = new() { Name = countryName, };
+
+        // Act
+        CountryResponse response = await _countryService.AddCountry(request);
+
+        // Assert
+        Assert.True(response.Id != Guid.Empty);
+    }
+    #endregion
+
+    #region GetAllCountries()
+    [Fact]
+    public async Task GetAllCountries_CountriesEmpty_ReturnEmptyList()
+    {
+        // Act
+        List<CountryResponse> actualResult = await _countryService.GetAllCountries();
+
+        // Assert
+        Assert.Empty(actualResult);
+    }
+
+    [Fact]
+    public async Task GetAllCountries_CountriesExist_ReturnSavedCountryList()
+    {
+        // Arrange
+        List<CountryAddRequest> countryAddRequestList = new()
+        {
+            new CountryAddRequest { Name = "America" },
+            new CountryAddRequest { Name = "Japan" },
+            new CountryAddRequest { Name = "Rusia" },
+        };
+
+        List<CountryResponse> expectedSavedCountryList = new();
+        foreach (var countryAddRequest in countryAddRequestList)
+        {
+            CountryResponse countryResponse = await _countryService.AddCountry(countryAddRequest);
+            expectedSavedCountryList.Add(countryResponse);
+        }
+
+        // Act
+        List<CountryResponse> result = await _countryService.GetAllCountries();
+
+        // Assert
+        foreach (var country in result)
+            Assert.Contains(country, expectedSavedCountryList);
+    }
+    #endregion
+
+    #region GetCountryById()
+    [Fact]
+    public async Task GetCountryById_IdNull_ReturnNull()
+    {
+        // Arrange
+        Guid? id = null;
+        
+        // Act
+        CountryResponse? result = await _countryService.GetCountryById(id);
+
+        // Assert
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task GetCountryById_Exist_ReturnCountryResponse()
+    {
+        // Arrange
+        CountryAddRequest countryAddRequest = new() { Name = "China" };
+        CountryResponse savedCountry = await _countryService.AddCountry(countryAddRequest);
+        Guid? savedCountryId = savedCountry.Id;
+
+        // Act
+        CountryResponse? result = await _countryService.GetCountryById(savedCountryId);
+        
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(savedCountry, result);
+    }
+
+    [Fact]
+    public async Task GetCountryById_NotExist_ReturnNull()
+    {
+        // Arrange
+        CountryAddRequest countryAddRequest = new() { Name = "China" };
+        await _countryService.AddCountry(countryAddRequest);
+        Guid unknownId = Guid.Parse("355558CB-285F-4272-AA72-F5ECFF18DD88");
+
+        // Act
+        CountryResponse? result = await _countryService.GetCountryById(unknownId);
+
+        // Assert
+        Assert.Null(result);
+    }
+    #endregion
+}
