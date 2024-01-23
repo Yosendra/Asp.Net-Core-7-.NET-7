03d063cf2c2f8b40223b3d068f51c03894c7c6cf
Yosendra <yosendra.evri@gmail.com>
2024-01-23 17:26:25 +0800

    Unit Testing - 08. Repository Implementation: Part 1

---
+++ 18. Unit Testing (Moq & Repository Pattern)/08. Repository Implementation - Part 1/Repositories/CountryRepository.cs
@@ -0,0 +1,38 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+
+namespace Repositories;
+
+public class CountryRepository : ICountryRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CountryRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Country> AddCountry(Country country)
+    {
+        _db.Countries.Add(country);
+        await _db.SaveChangesAsync();
+        
+        return country;
+    }
+
+    public async Task<List<Country>> GetAllCountries()
+    {
+        return await _db.Countries.ToListAsync();
+    }
+
+    public async Task<Country?> GetCountryById(Guid id)
+    {
+        return await _db.Countries.FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<Country?> GetCountryByName(string name)
+    {
+        return await _db.Countries.FirstOrDefaultAsync(c => c.Name == name);
+    }
+}
---
+++ 18. Unit Testing (Moq & Repository Pattern)/08. Repository Implementation - Part 1/RepositoryContracts/IPersonRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq.Expressions;
+using Entities;
+
+namespace RepositoryContracts;
+
+/// <summary>
+/// Represents data access logic for managin Person entity
+/// </summary>
+public interface IPersonRepository
+{
+    /// <summary>
+    /// Add a person object to the data store
+    /// </summary>
+    /// <param name="person">Person object to add</param>
+    /// <returns>Return the person object after adding it to the table</returns>
+    Task<Person> AddPerson(Person person);
+
+    /// <summary>
+    /// Returns all persons in the data store
+    /// </summary>
+    /// <returns>List of person object from table</returns>
+    Task<List<Person>> GetAllPersons();
+
+    /// <summary>
+    /// Returns a person object based on the given id
+    /// </summary>
+    /// <param name="id">Id to search</param>
+    /// <returns>Person object or null</returns>
+    Task<Person> GetPersonById(Guid id);
+
+    /// <summary>
+    /// Returns all person object based on the given expression
+    /// </summary>
+    /// <param name="predicate">LINQ expression to check</param>
+    /// <returns>All matching person with given condition</returns>
+    Task<List<Person>> GetFilteredPerson(Expression<Func<Person, bool>> predicate);
+
+    /// <summary>
+    /// Delete a person object based on the given id
+    /// </summary>
+    /// <param name="id">Id to search</param>
+    /// <returns>Returns true, if deletion success, otherwise false</returns>
+    Task<bool> DeletePersonById(Guid id);
+
+    /// <summary>
+    /// Update a person object (person name and other details) based on the given id
+    /// </summary>
+    /// <param name="person">Person object to update</param>
+    /// <returns>Returns the updated person object</returns>
+    Task<Person> UpdatePerson(Person person);
+}
