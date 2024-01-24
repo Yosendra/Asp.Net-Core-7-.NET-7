3b9ef17623dce4f1a73d87394c6a60d67a417034
Yosendra <yosendra.evri@gmail.com>
2024-01-24 23:46:15 +0800

    Unit Testing - 13. Mock Repository: Part 1

---
+++ 18. Unit Testing (Moq & Repository Pattern)/13. Mock Repository - Part 1/CRUDExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Mock Repository
+ *   
+ *   Look at: PersonServiceTest.cs
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
