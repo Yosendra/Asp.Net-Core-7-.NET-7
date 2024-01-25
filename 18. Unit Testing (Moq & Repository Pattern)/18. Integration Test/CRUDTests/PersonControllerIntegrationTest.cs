6873d684629737d638ea9223e56f6529bbe4a8bd
Yosendra <yosendra.evri@gmail.com>
2024-01-26 01:09:50 +0800

    Unit Testing - 18. Integration Test

---
+++ 18. Unit Testing (Moq & Repository Pattern)/18. Integration Test/CRUDTests/PersonControllerIntegrationTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace CRUDTests;
+
+public class PersonControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+{
+    // we neet to create httpClient object based on our WebApplicationFactory because any request we make through httpClient
+    // should be received by our web application
+    private readonly HttpClient _httpClient;
+
+    public PersonControllerIntegrationTest(CustomWebApplicationFactory factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    #region Index
+
+    [Fact]
+    public async Task Index_ReturnView()
+    {
+        // Arrange
+
+        // Act
+        HttpResponseMessage response = await _httpClient.GetAsync("/Person/Index");
+
+        // Assert
+        response.Should().BeSuccessful();
+    }
+
+    #endregion
+
+
+}
---
+++ 18. Unit Testing (Moq & Repository Pattern)/18. Integration Test/Entities/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities;
+
+/// <summary>
+/// Person domain model class
+/// </summary>
+public class Person
+{
+    [Key]
+    public Guid? Id { get; set; }
+
+    [StringLength(40)]
+    public string? Name { get; set; }
+
+    [StringLength(40)]
+    public string? Email { get; set; }
+
+    public DateTime? DateOfBirth { get; set; }
+
+    [StringLength(10)]
+    public string? Gender { get; set; }
+
+    [StringLength(200)]
+    public string? Address { get; set; }
+
+    public bool ReceiveNewsLetters { get; set; }
+
+    public string? TIN { get; set; }
+
+    public Guid? CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    public virtual Country? Country { get; set; }
+}
