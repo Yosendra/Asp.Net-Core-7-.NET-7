be1863031c15f6af52cbc30d3c58b538fce613a6
Yosendra <yosendra.evri@gmail.com>
2024-01-25 20:56:12 +0800

    Unit Testing - 16. Controller Unit Test: Part 1

---
+++ 18. Unit Testing (Moq & Repository Pattern)/16. Controller Unit Test - Part 1/CRUDExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Controller Unit Test
+ *   Runtime      :              Controller -> Service -> Repository -> DbContext  
+ *   Unit Testing : Unit Test -> Controller -> Service Mock
+ *   
+ *   Add reference project to CRUDExample in CrudTest project
+ *   
+ *   Look at: PersonControllerTest.cs (not complete)
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
---
+++ 18. Unit Testing (Moq & Repository Pattern)/16. Controller Unit Test - Part 1/CRUDTests/PersonControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using AutoFixture;
+using CRUDExample.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+
+namespace CRUDTests;
+
+public class PersonControllerTest
+{
+    private readonly IPersonService _personService;
+    private readonly ICountryService _countryService;
+
+    private readonly Mock<IPersonService> _personServiceMock;
+    private readonly Mock<ICountryService> _countryServiceMock;
+
+    private readonly IFixture _fixture;
+
+    public PersonControllerTest()
+    {
+        _fixture = new Fixture();
+        _personServiceMock = new Mock<IPersonService>();
+        _countryServiceMock = new Mock<ICountryService>();
+
+        _personService = _personServiceMock.Object;
+        _countryService = _countryServiceMock.Object;
+    }
+
+    [Fact]
+    public async Task Index_ShouldReturnIndexViewWithPersonList()
+    {
+        // Arrange
+        List<PersonResponse> personResponseList = _fixture.Create<List<PersonResponse>>();
+
+        _personServiceMock
+            .Setup(service => service.GetFilteredPersons(It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(personResponseList);
+
+        _personServiceMock
+            .Setup(service => service.GetSortedPersons(It.IsAny<List<PersonResponse>>(), It.IsAny<string>(), It.IsAny<SortOrderEnum>()))
+            .ReturnsAsync(personResponseList);
+
+        PersonController personController = new(_personService, _countryService);
+
+        string searchBy = _fixture.Create<string>();
+        string? keyword = _fixture.Create<string>();
+        string sortBy = _fixture.Create<string>();
+        SortOrderEnum sortOrder = _fixture.Create<SortOrderEnum>();
+
+        // Act
+        IActionResult result = await personController.Index(searchBy, keyword, sortBy, sortOrder);
+
+        // Assert
+        ViewResult viewResult = Assert.IsType<ViewResult>(result);
+        viewResult.ViewData.Model.Should().BeAssignableTo<IEnumerable<PersonResponse>>();
+        viewResult.ViewData.Model.Should().Be(personResponseList);
+    }
+}
