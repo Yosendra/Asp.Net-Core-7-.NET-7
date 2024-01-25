385042a2921142a803304d98747acbb638bc5f1b
Yosendra <yosendra.evri@gmail.com>
2024-01-25 16:33:04 +0800

    Unit Testing - 14. Mock Repository: Part 2

---
+++ 18. Unit Testing (Moq & Repository Pattern)/14. Mock Repository - Part 2/CRUDExample/Controllers/CountryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace CRUDExample.Controllers;
+
+[Route("[controller]")]
+public class CountryController : Controller
+{
+    private readonly ICountryService _countryService;
+
+    public CountryController(ICountryService countryService)
+    {
+        _countryService = countryService;
+    }
+
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult UploadFromExcel()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("[action]")]
+    public async Task<IActionResult> UploadFromExcel(IFormFile excelFile) // notice the type, and the name must be same as the name in the form
+    {
+        if (excelFile == null || excelFile.Length == 0)
+        {
+            ViewBag.ErrorMessage = "Please select an excel file";
+            return View();
+        }
+
+        if (!Path.GetExtension(excelFile.FileName).Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+        {
+            ViewBag.ErrorMessage = "Unsupported file. '.xlsx' file is expected";
+            return View();
+        }
+
+        int countCountryInserted = await _countryService.UploadCountryFromExcelFile(excelFile);
+        
+        ViewBag.Message = $"{countCountryInserted} countries uploaded";
+        
+        return View();
+    }
+}
---
+++ 18. Unit Testing (Moq & Repository Pattern)/14. Mock Repository - Part 2/CRUDExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Mock Repository
+ *   
+ *   Test still not complete in PersonTest
+ *   
+ *   Look at: PersonServiceTest.cs at AddPerson_Success_ReturnPersonResponseWithId()
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
