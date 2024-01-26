fc218e66176fbd7d01c434cdcb77c05de0ad29df
Yosendra <yosendra.evri@gmail.com>
2024-01-26 08:20:25 +0800

    Logging - 04. Logging Providers

---
+++ 19. Logging & Serilog/04. Logging Providers/CRUDExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+
+/* Logging Providers (Console, Debug, EventLog)
+
+    Log automatically displayed in Console (Kestrel Console) and Debug (In Visual Studio -> Output -> Debug)
+
+    We try to change log level for EventLog, we customize it in appsettings.Development.json -> EventLog:LogLevel:Default
+
+    We customize our log provider in Program.cs
+
+Look at: appsettings.Development.json, Program.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Logging
+builder.Host.ConfigureLogging(loggingProvider =>
+{
+    loggingProvider.ClearProviders();   // Remove all Logging provider (Debug, Console, EventLog)
+    loggingProvider.AddConsole();       // Add Console as our Logging provider, now Console is the only one place we can see the log
+});
+
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")))
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+// All these messages can be seen in Kestrel console when running the web application
+app.Logger.LogDebug("debug-message");   // except debug and trace because the default log level of asp.net core application is set to information
+app.Logger.LogInformation("information-message");
+app.Logger.LogWarning("warning-message");
+app.Logger.LogError("error-message");
+app.Logger.LogCritical("critical-message");
+
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
---
+++ 19. Logging & Serilog/04. Logging Providers/Repositories/PersonRepository.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq.Expressions;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+
+namespace Repositories;
+
+public class PersonRepository : IPersonRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public PersonRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Person> AddPerson(Person person)
+    {
+        _db.Persons.Add(person);
+        await _db.SaveChangesAsync();
+
+        return person;
+    }
+
+    public async Task<bool> DeletePersonById(Guid id)
+    {
+        Person? person = await _db.Persons.SingleOrDefaultAsync(p => p.Id == id);
+        if (person == null)
+            return false;
+
+        _db.Persons.Remove(person);
+        await _db.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task<List<Person>> GetAllPersons()
+    {
+        return await _db.Persons
+            .Include(p => p.Country)
+            .ToListAsync();
+    }
+
+    public async Task<List<Person>> GetFilteredPerson(Expression<Func<Person, bool>> predicate)     // notice this is quite unique
+    {
+        return await _db.Persons
+            .Include(p => p.Country)
+            .Where(predicate)
+            .ToListAsync();
+    }
+
+    public async Task<Person?> GetPersonById(Guid id)
+    {
+        return await _db.Persons
+            .Include(p => p.Country)
+            .SingleOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<Person?> UpdatePerson(Person person)
+    {
+        Person? matchingPerson = await _db.Persons.SingleOrDefaultAsync(p => p.Id == person.Id);
+        if (matchingPerson == null)
+            return person;
+
+        matchingPerson.Name = person.Name;
+        matchingPerson.Email = person.Email;
+        matchingPerson.DateOfBirth = person.DateOfBirth;
+        matchingPerson.Gender = person.Gender;
+        matchingPerson.Address = person.Address;
+        matchingPerson.ReceiveNewsLetters = person.ReceiveNewsLetters;
+        matchingPerson.CountryId = person.CountryId;
+
+        await _db.SaveChangesAsync();
+
+        return matchingPerson;
+    }
+}
