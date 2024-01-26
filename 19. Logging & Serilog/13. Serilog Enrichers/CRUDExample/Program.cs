c67e3bf1d05850d219d12d07d48265b2beb34bc4
Yosendra <yosendra.evri@gmail.com>
2024-01-27 06:24:04 +0800

    Logging - 13. Serilog Enrichers

---
+++ 19. Logging & Serilog/13. Serilog Enrichers/CRUDExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Serilog Enrichers
+    Enrichers are additional details that are added to LogContext, so they are stored in logs.
+    Eg: MachineName [or] Custom Property
+
+                                  Log 1                           Log 1 (enrichers)
+            Request               --------------------->          --------------------->
+    Browser -------> Asp.Net Core                         Serilog                           Serilog Sink
+                                  Log 2                           Log 2 (enrichers)
+                                  --------------------->          --------------------->
+    
+    We add "Enrich" configuration to add our custom key "ApplicationName" in our log context
+    Now "ApplicationName" will be added to all our log.
+
+Look at: appsettings.json
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services
+    .AddHttpLogging(configureOptions => 
+    {
+        configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+    })
+    .AddDbContext<ApplicationDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    })
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
