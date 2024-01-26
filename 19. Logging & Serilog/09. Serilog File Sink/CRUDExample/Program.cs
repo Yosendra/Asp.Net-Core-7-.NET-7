56324580a22680a2eb366edeb5949fd331a0a5d7
Yosendra <yosendra.evri@gmail.com>
2024-01-27 00:19:53 +0800

    Logging - 09. Serilog File Sink

---
+++ 19. Logging & Serilog/09. Serilog File Sink/CRUDExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Serilog - File Sink
+    
+    We use File as the sink by adding "Serilog.Sinks.File" in Serilog configuration in appsettings.json
+    
+    The "Serilog.Sinks.File" logs into a specified file.
+    You can configure the filename, rolling interval, file size limit etc., using configuration settings.
+
+    .Net Core App ---> Serilog ---> File Logs
+
+    We add some configuration for the path of logging file in Serilog in "WritTo" key
+
+    You may run the application and see the log file in "logs" directory
+
+Look at: appsettings.json
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services
+    .AddHttpLogging(configureOptions => 
+    {
+        configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+    })
+    .AddDbContext<ApplicationDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    })
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
---
+++ 19. Logging & Serilog/09. Serilog File Sink/Services/CountryService.cs
@@ -0,0 +1,99 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+using RepositoryContracts;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services;
+
+public class CountryService : ICountryService
+{
+    private readonly ICountryRepository _countryRepository;
+
+    public CountryService(ICountryRepository countryRepository)
+    {
+        _countryRepository = countryRepository;
+    }
+
+    public async Task<CountryResponse> AddCountry(CountryAddRequest? requestModel)
+    {
+        ArgumentNullException.ThrowIfNull(requestModel, nameof(requestModel));
+
+        if (requestModel.Name == null)
+        {
+            string errorMessage = string.Format("{0} cannot be null.", nameof(requestModel.Name));
+            throw new ArgumentException(errorMessage);
+        }
+
+        requestModel.Name = requestModel.Name.Trim();
+        Country? country = await _countryRepository.GetCountryByName(requestModel.Name);
+        if (country != null)
+        {
+            string errorMessage = string.Format("{0} country is already exist.", requestModel.Name);
+            throw new ArgumentException(errorMessage);
+        }
+
+        country = requestModel.ToCountry();
+        country.Id = Guid.NewGuid();
+        await _countryRepository.AddCountry(country);
+
+        return country.ToCountryResponse();
+    }
+
+    public async Task<List<CountryResponse>> GetAllCountries()
+    {
+        var countryList = await _countryRepository.GetAllCountries();
+
+        return countryList.Select(country => country.ToCountryResponse()).ToList();
+    }
+
+    public async Task<CountryResponse?> GetCountryById(Guid? id)
+    {
+        if (id == null) return null;
+
+        Country? country = await _countryRepository.GetCountryById(id.Value);
+
+        return country?.ToCountryResponse();
+    }
+
+    public async Task<int> UploadCountryFromExcelFile(IFormFile formFile)
+    {
+        MemoryStream memoryStream = new();
+        await formFile.CopyToAsync(memoryStream);
+
+        using ExcelPackage excelPackage = new(memoryStream);
+        ExcelWorksheet workSheet = excelPackage.Workbook.Worksheets["Country"];
+
+        int rowCount = workSheet.Dimension.Rows,
+            countryInserted = 0;
+
+        /* row index in excel start from '1'
+         * '1' used for header
+         * '2' and so on is the actual data
+         */
+        for (int row = 2; row <= rowCount; row++)
+        {
+            string? cellValue = Convert.ToString(workSheet.Cells[row, 1].Value);
+            if (!string.IsNullOrWhiteSpace(cellValue))
+            {
+                string countryName = cellValue;
+                Country? country = await _countryRepository.GetCountryByName(countryName);
+                bool isCountryExist = country != null;
+                if (!isCountryExist)
+                {
+                    country = new()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = countryName,
+                    };
+                    await _countryRepository.AddCountry(country);
+                    
+                    countryInserted++;
+                }
+            }
+        }
+
+        return countryInserted;
+    }
+}
