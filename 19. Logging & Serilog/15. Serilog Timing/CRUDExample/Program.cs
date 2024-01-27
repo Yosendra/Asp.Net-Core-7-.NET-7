f92357cb4c2a4da9f75f425288621f4bb5d32bcb
Yosendra <yosendra.evri@gmail.com>
2024-01-27 20:42:05 +0800

    Logging - 15. Serilog Timing

---
+++ 19. Logging & Serilog/15. Serilog Timing/CRUDExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Serilog Timing
+    "SerilogTimings" package records timing of a piece of your source code, indicating how much time taken for executing it.
+
+    SerilogTimings -> Start
+                       |
+                       | Time taken : 00:01 second
+                       |
+    SerilogTimings -> End
+
+    Install package "SerilogTimings" in Service project
+
+    In PersonService, in GetFilteredPersons(), we want to record the time execution.
+
+    We fix PersonServiceTest for lacking of ILogger and IDiagnosticContext injection in the constructor
+
+Look at: PersonServiceTest.cs, PersonService.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services
+    .AddHttpLogging(configureOptions => 
+    {
+        configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+    })
+    .AddDbContext<ApplicationDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    })
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
---
+++ 19. Logging & Serilog/15. Serilog Timing/ServiceContracts/ICountryService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using ServiceContracts.DTO;
+
+namespace ServiceContracts;
+
+/// <summary>
+/// Represents business logic for manipulating Country entity
+/// </summary>
+public interface ICountryService
+{
+    /// <summary>
+    /// Adds a country object to the list of countries
+    /// </summary>
+    /// <param name="countryAddRequest">Country object to add</param>
+    /// <returns>Returns the country object after adding it (including newly generated country id)</returns>
+    Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequest);
+
+    /// <summary>
+    /// Return all countries from the list
+    /// </summary>
+    /// <returns>All countries from the list as List of CountryResponse</returns>
+    Task<List<CountryResponse>> GetAllCountries();
+
+    /// <summary>
+    /// Returns a country object based on the given country id
+    /// </summary>
+    /// <param name="id">Id (Guid) to search</param>
+    /// <returns>Matching country as CountryResponse object</returns>
+    Task<CountryResponse?> GetCountryById(Guid? id);
+
+    /// <summary>
+    /// Upload countries from excel file into database
+    /// </summary>
+    /// <param name="formFile">Excel file with list of countries</param>
+    /// <returns>Return number of countries added</returns>
+    Task<int> UploadCountryFromExcelFile(IFormFile formFile);
+}
