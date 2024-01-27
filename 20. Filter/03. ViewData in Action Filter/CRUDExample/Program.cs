64775b6a3b0f1269f81aa142bee5a928fe36850c
Yosendra <yosendra.evri@gmail.com>
2024-01-28 07:57:26 +0800

    Filter - 03. ViewData in Action Filter

---
+++ 20. Filter/03. ViewData in Action Filter/CRUDExample/Program.cs
@@ -0,0 +1,50 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Manipulating ViewData in Action Filter
+
+  We want add data in the ViewBag in ActionFilter after executing the action method
+  On OnActionExecuted method in PersonListActionFilter class, we add certain key to the ViewBag
+
+  There is some interesting code in OnActionExecuting to enable accessing argument at OnActionExecuted
+
+Look at: PersonListActionFilter.cs, 
+         PersonController.cs (we move the logic for manipulating ViewBag to OnActionExecuted in PersonListActionFilter class)
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services
+    .AddHttpLogging(configureOptions => 
+    {
+        configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+    })
+    .AddDbContext<ApplicationDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    })
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
