c71ff65871fd0e9dd2b33b916618ebf8aa620a2b
Yosendra <yosendra.evri@gmail.com>
2024-01-28 23:45:36 +0800

    Filter - 04. Serilog Structured Logging

---
+++ 20. Filter/04. Serilog Structured Logging/CRUDExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using CRUDExample.Bootstrap;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Serilog Structured Logging 
+
+  Look at how we give logging message at OnActionExecuted() in PersonListActionFilter
+  We structured it, so it can be dynamic using nameof()
+  
+  You may run the program and at Seq monitoring
+  You can search by typing this FilterName = "PersonListActionFilter" to get our log in ActionFilter
+  Notice the "FilterName" we define it when giving logging message in ActionFilter
+
+Look at: PersonListActionFilter.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services
+    .AddHttpLogging(configureOptions => 
+    {
+        configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+    })
+    .AddDbContext<ApplicationDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    })
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews();
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
---
+++ 20. Filter/04. Serilog Structured Logging/CRUDTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRUDTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.UseEnvironment("IntegrationTest");
+
+        builder.ConfigureServices(services => 
+        {
+            var descriptor = services.SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+            {
+                options.UseInMemoryDatabase("DatabaseForTesting");
+            });
+        });
+    }
+}
