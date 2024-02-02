6619a207f6dcaf5e6c2c1ef4dfa2784f5332d304
Yosendra <yosendra.evri@gmail.com>
2024-02-02 21:21:03 +0800

    Filter - 09. Async Filters

---
+++ 20. Filter/09. Async Filters/CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ActionFilters;
+
+public class ResponseHeaderActionFilter : IAsyncActionFilter, IOrderedFilter            // notice here, we implements "IAsyncActionFilter" instead of "IActionFilter"
+{
+    private readonly ILogger<ResponseHeaderActionFilter> _logger;
+    private readonly string Key;
+    private readonly string Value;
+
+    public int Order { get; set; }
+
+    public ResponseHeaderActionFilter(
+        ILogger<ResponseHeaderActionFilter> logger,
+        string key,
+        string value,
+        int order)
+    {
+        _logger = logger;
+        Key = key;
+        Value = value;
+        Order = order;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)                      // Notice this
+    {
+        _logger.LogInformation("{FilterName}.{MethodName} method - BEFORE",         // The logic for BEFORE
+           nameof(PersonListActionFilter),
+           nameof(OnActionExecutionAsync));
+        context.HttpContext.Response.Headers[Key] = Value;
+
+        await next();                                                               // calls the subsequent filter or action method
+
+        _logger.LogInformation("{FilterName}.{MethodName} method - AFTER",          // The logic for AFTER
+            nameof(ResponseHeaderActionFilter),
+            nameof(OnActionExecutionAsync));
+    }
+}
---
+++ 20. Filter/09. Async Filters/CRUDExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using CRUDExample.Bootstrap;
+using CRUDExample.Filters.ActionFilters;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* 
+  If you want to perform asynchronous operation in the filter's method, then implements "IAsyncActionFilter" instead.
+  
+  public async Task OnActionExecutionAsync(ActionExecuting context, ActionExecutionDelegate next)
+  {
+    // Logic for BEFORE
+    await next();
+    // Logic for AFTER
+  }
+
+  We will convert our custom filter "ResponseHeaderActionFilter" into async filter
+
+Look at: ResponseHeaderActionFilter.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+    {
+        loggerConfiguration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(service);
+    });
+builder.Services
+    .AddHttpLogging(configureOptions => 
+    {
+        configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+    })
+    .AddDbContext<ApplicationDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    })
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews(options =>
+    {
+        var logger = builder.Services
+            .BuildServiceProvider()
+            .GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+
+        options.Filters.Add(new ResponseHeaderActionFilter(logger, "Key-From-Global", "Value-From-Global", 2));
+    });
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
