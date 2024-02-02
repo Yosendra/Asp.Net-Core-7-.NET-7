9a2e854329f2e3443e37916c57c3c0c3a6d7f97a
Yosendra <yosendra.evri@gmail.com>
2024-02-03 03:31:36 +0800

    Filter - 10. Short Circuiting Action Filter

---
+++ 20. Filter/10. Short Circuiting Action Filter/CRUDExample/Filters/ActionFilters/PersonCreateAndEditPostActionFilter.cs
@@ -0,0 +1,53 @@
+﻿using CRUDExample.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace CRUDExample.Filters.ActionFilters;
+
+public class PersonCreateAndEditPostActionFilter : IAsyncActionFilter
+{
+    private readonly ICountryService _countryService;
+
+    public PersonCreateAndEditPostActionFilter(ICountryService countryService)
+    {
+        _countryService = countryService;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        // BEFORE
+        if (context.Controller is PersonController controller)
+        {
+            if (!controller.ModelState.IsValid)
+            {
+                List<CountryResponse> countries = await _countryService.GetAllCountries();
+                controller.ViewBag.Countries = countries.Select(c => new SelectListItem { Text = c.Name, Value = c.Id.ToString() });
+                controller.ViewBag.Errors = controller.ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                // Notice this, to get the argument value in the action
+                var requestModel = context.ActionArguments["requestModel"];
+
+                // As soon you assign non-null value to the Result, it considered we are short-circuiting the action method
+                // It means the subsequent filter, as well as the actual action method will not be executed
+                context.Result = controller.View(requestModel);
+
+                // Because we are short-circuiting, we should not invoke the "next" delegate
+            }
+            else
+            {
+                await next();
+            }
+        }
+        else
+        {
+            await next();
+        }
+
+        // AFTER
+    }
+}
---
+++ 20. Filter/10. Short Circuiting Action Filter/CRUDExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using CRUDExample.Bootstrap;
+using CRUDExample.Filters.ActionFilters;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/*
+ * When a filter shor-circuit (by assigning "Result" property of the context), it skips all remaining filters
+ * and action method, and directly sends the given result as response to the browser.
+ 
+  Create short-circuit filter PersonCreateAndEditPostActionFilter for validation in Person Create & Edit (POST) method
+  We move the model validation logic of those two actions (POST) into the filter
+  Use PersonCreateAndEditPostActionFilter in our those methods mentioned above
+
+  Commented the validation javascript in Create person view for testing purpose
+
+  Insight of Short-Circuit : giving the result from the filter and preventing the action method from execution
+
+Look at: PersonCreateAndEditPostActionFilter.cs, PersonController.Create (POST), PersonController.Edit (POST), 
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+    {
+        loggerConfiguration
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(service);
+    });
+builder.Services
+    .AddHttpLogging(configureOptions => 
+    {
+        configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+    })
+    .AddDbContext<ApplicationDbContext>(options => 
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    })
+    .AddRepositories()
+    .AddServices()
+    .AddControllersWithViews(options =>
+    {
+        var logger = builder.Services
+            .BuildServiceProvider()
+            .GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+
+        options.Filters.Add(new ResponseHeaderActionFilter(logger, "Key-From-Global", "Value-From-Global", 2));
+    });
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
