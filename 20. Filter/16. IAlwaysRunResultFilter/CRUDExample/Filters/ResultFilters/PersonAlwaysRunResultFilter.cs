575b92bc5aad01f411bb05e6fb2e0b2c8fbceb9e
Yosendra <yosendra.evri@gmail.com>
2024-02-04 21:42:31 +0800

    Filter - 16. IAlwaysRunResultFilter

---
+++ 20. Filter/16. IAlwaysRunResultFilter/CRUDExample/Filters/ResultFilters/PersonAlwaysRunResultFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ResultFilters;
+
+public class PersonAlwaysRunResultFilter : IAlwaysRunResultFilter
+{
+    public void OnResultExecuting(ResultExecutingContext context)
+    {
+
+    }
+
+    public void OnResultExecuted(ResultExecutedContext context)
+    {
+        
+    }
+}
---
+++ 20. Filter/16. IAlwaysRunResultFilter/CRUDExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using CRUDExample.Bootstrap;
+using CRUDExample.Filters.ActionFilters;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* IAlwaysRunResultFilter
+* 
+* case: We want to test, is AlwaysRunResultFilter always be executed
+* 
+* Create PersonAlwaysRunResultFilter, implements IAlwaysRunResultFilter
+* Apply PersonAlwaysRunResultFilter at Edit (POST) in PersonController
+* Comment out the TokenResultFilter at Edit (GET) in PersonController
+* Put breakpoint at PersonAlwaysRunResultFilter on both methods
+* Run the application
+* Go to Edit person view, then submit the form
+* 
+* Look at: PersonAlwaysRunResultFilter.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.AddHttpLogging(configureOptions => 
+{
+    configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+})
+.AddDbContext<ApplicationDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+})
+.AddRepositories()
+.AddServices()
+.AddControllersWithViews(options => 
+{
+    var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+    options.Filters.Add(new ResponseHeaderActionFilter(logger, "Key-From-Global", "Value-From-Global", 2));
+});
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
