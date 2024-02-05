03dd7a5f852ad2e7d97ca29134f428bbd44bca65
Yosendra <yosendra.evri@gmail.com>
2024-02-05 19:05:32 +0800

    Filter - 20. IFilterFactory

---
+++ 20. Filter/20. IFilterFactory/CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ActionFilters;
+
+// New class that implements IFilterFactory
+public class ResponseHeaderFilterFactoryAttribute : Attribute, IFilterFactory
+{
+    private string Key { get; set; }
+    private string Value { get; set; }
+    private int Order { get; set; }
+
+    // True means filter object can be accessible across multiple request in the application
+    public bool IsReusable => false;
+
+    public ResponseHeaderFilterFactoryAttribute(
+        string key,
+        string value,
+        int order)
+    {
+        Key = key;
+        Value = value;
+        Order = order;
+    }
+
+    // Controller -> FilterFactory -> Filter
+    public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+    {
+        // return filter object
+
+        //var filter = new ResponseHeaderActionFilter(Key, Value, Order);
+        var filter = serviceProvider.GetRequiredService<ResponseHeaderActionFilter>();      // get the filter object through IoC container instead, remember to register it in IoC container
+
+        // This is the way we can give our constructor argument to the filter, that's why we make it public property ResponseHeaderActionFilter
+        filter.Key = Key;
+        filter.Value = Value;
+        filter.Order = Order;
+
+        return filter;
+    }
+}
+
+
+//public class ResponseHeaderActionFilter : ActionFilterAttribute
+public class ResponseHeaderActionFilter : IAsyncActionFilter, IOrderedFilter            // notice this
+{
+    //private readonly string Key;
+    //private readonly string Value;
+    
+    // Make these all property
+    public string Key { get; set; }
+    public string Value { get; set; }
+    public int Order { get; set; }
+
+    private readonly ILogger<ResponseHeaderActionFilter> _logger;
+
+    //public ResponseHeaderActionFilter(
+    //    string key,
+    //    string value,
+    //    int order)
+    //{
+    //    Key = key;
+    //    Value = value;
+    //    Order = order;
+    //}
+
+    // Now we can inject our service though constructor DI
+    public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        // BEFORE
+        _logger.LogInformation("BEFORE - ResponseHeaderActionFilter");
+
+        context.HttpContext.Response.Headers[Key] = Value;
+
+        await next();
+
+        // AFTER
+    }
+}
---
+++ 20. Filter/20. IFilterFactory/CRUDExample/Program.cs
@@ -0,0 +1,70 @@
+﻿using CRUDExample.Bootstrap;
+using CRUDExample.Filters.ActionFilters;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* IFilterFactory
+* 
+* case: We want our ResponseHeaderActionFilter attribute to also support contructor DI. We can do that with IFilterFactory
+* 
+* We modify our ResponseHeaderActionFilter
+* Add new class ResponseHeaderFilterFactoryAttribute in the same namespace as ResponseHeaderActionFilter
+* Make ResponseHeaderFilterFactoryAttribute inherits from Attribute class & implements IFilterFactory interface
+* Long explanation, just see the code in these files listed below
+* 
+* Best practice in real world project, avoid using action filter attribute or result filter attribute
+* Either you can use IFilterFactory or TypeFilter or ServiceFilter
+* FilterAttribute is not recomended because it is not supporting constructor injection
+* That's why if you still want to use FilterAttribute then use the IFilterFactory
+* 
+* Look at: ResponseHeaderActionFilter.cs, ResponseHeaderFilterFactoryAttribute class, PersonController.cs, Program.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+
+builder.Services.AddTransient<PersonListActionFilter>();
+builder.Services.AddTransient<ResponseHeaderActionFilter>();            // don't forget to register ResponseHeaderActionFilter into ServiceCollection
+builder.Services.AddHttpLogging(configureOptions =>
+{
+    configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+});
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddControllersWithViews(options => 
+{
+    //options.Filters.Add(new ResponseHeaderActionFilter("Key-From-Global", "Value-From-Global", 2));
+
+    var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();         // notice this
+    options.Filters.Add(new ResponseHeaderActionFilter(logger)
+    {
+        Key = "Key-From-Global",
+        Value = "Value-From-Global",
+        Order = 2,
+    });
+});
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
