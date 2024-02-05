71a278b61171220b09583b64c62728b4ce398ac7
Yosendra <yosendra.evri@gmail.com>
2024-02-06 02:11:39 +0800

    Filter - 24. Configure Services Extension

---
+++ 20. Filter/24. Configure Services Extension/CRUDExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using CRUDExample;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Configure Services Extension
+* 
+* Create extension method for grouping register service logic inside ConfigureServices() method at ConfigureServicesExtension class
+* 
+* Look at: ConfigureServicesExtension.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.ConfigureServices(builder.Configuration);           // Invoke our extension method
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
