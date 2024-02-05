b9fa9303191d43ac484ecbb2077c22672a20ec4c
Yosendra <yosendra.evri@gmail.com>
2024-02-06 05:19:25 +0800

    Error Handling - 01. Exception Handling Middleware

---
+++ 21. Error Handling/01. Exception Handling Middleware/CRUDExample/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace CRUDExample.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        private readonly IDiagnosticContext _diagnosticContext;
+
+        public ExceptionHandlingMiddleware(
+            RequestDelegate next, 
+            ILogger<ExceptionHandlingMiddleware> logger, 
+            IDiagnosticContext diagnosticContext)
+        {
+            _next = next;   // represents the subsequent middleware
+            _logger = logger;
+            _diagnosticContext = diagnosticContext;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);       // surround it inside try-catch statement, so all the error can be catched
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    _logger.LogError("{ExceptionType} {ExceptionMessage}", 
+                        ex.InnerException.GetType().ToString(), 
+                        ex.InnerException.Message);
+                }
+                else
+                {
+                    _logger.LogError("{ExceptionType} {ExceptionMessage}", 
+                        ex.GetType().ToString(), 
+                        ex.Message);
+                }
+            }
+
+            // Write custom exception message to the response because we don't want to view
+            // the detail of the error to the user of production environment
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await httpContext.Response.WriteAsync("Error occured");
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
---
+++ 21. Error Handling/01. Exception Handling Middleware/CRUDExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using CRUDExample;
+using CRUDExample.Middleware;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Exception Handling Middleware
+* 
+* Handles all errors occured in filter pipeline (including model binding, controllers, and filters)
+* Should be added to the application pipeline before Routing Middleware
+* 
+* Exception Handling Middleware -> Other Middleware -> Routing Middleware (action selection) -> Endpoint Middleware (action execution)
+* 
+* Inside Endpoint Middleware
+* Filter Pipeline : Filter (before) -> Action method -> Fitler (after)
+* 
+* 
+* Test : simulate exception by giving wrong database name in appsettings.json
+* Comment out HandleExceptionFilter filter at PersonController
+* Change the environment ASPNETCORE_ENVIRONMENT value to "Production"
+* The error page will be 500 internal server error, empty page without message to trace, not developer friendly
+* 
+* Error Handling Filter is limited to only specific request 
+* We want to handle this error in application-level scope (all request) by using Error Handling Middleware
+* 
+* Create our error handling middleware, ExceptionHandlingMiddleware class
+* Use this middleware in Program.cs
+* Apply the middleware at most top middleware, this is a must, to make all exception can be catched by the middleware
+* Put the breakpoint at the error handling middleware, then run the application
+* 
+* Look at: appsettings.json, ExceptionHandlingMiddleware.cs, Program.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.ConfigureServices(builder.Configuration);
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandlingMiddleware();       // Apply our ErrorHandlingMiddleware beside Development environment
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
