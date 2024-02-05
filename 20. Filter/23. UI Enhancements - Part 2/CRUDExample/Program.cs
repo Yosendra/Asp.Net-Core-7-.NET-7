1bc13bb85faad2e1923442796c0c53c86aad11e5
Yosendra <yosendra.evri@gmail.com>
2024-02-05 23:20:32 +0800

    Filter - 23. UI Enhancements: Part 2

---
+++ 20. Filter/23. UI Enhancements - Part 2/CRUDExample/Program.cs
@@ -0,0 +1,67 @@
+﻿using CRUDExample.Bootstrap;
+using CRUDExample.Filters.ActionFilters;
+using Entities;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* UI Enchancement
+* 
+* Enhance the css layout and html template for the layout 
+* 
+* Look at: _Layout.cshtml, Index.cshtml view of PersonController
+* 
+* Continue enchance the view
+* Adding the else condition at PersonListActionFilter for ViewBag
+* 
+* Look at: Create.cshtml, Edit.cshtml, Delete.cshtml view of PersonController
+*          UploadFromExcel.cshtml view of CountryController
+* 
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+
+builder.Services.AddTransient<PersonListActionFilter>();
+builder.Services.AddTransient<ResponseHeaderActionFilter>();
+builder.Services.AddHttpLogging(configureOptions =>
+{
+    configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+});
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddControllersWithViews(options => 
+{
+    var logger = builder.Services
+        .BuildServiceProvider()
+        .GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+    options.Filters.Add(new ResponseHeaderActionFilter(logger)
+    {
+        Key = "Key-From-Global",
+        Value = "Value-From-Global",
+        Order = 2,
+    });
+});
+
+var app = builder.Build();
+app.UseSerilogRequestLogging();
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
---
+++ 20. Filter/23. UI Enhancements - Part 2/Entities/Person.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities;
+
+/// <summary>
+/// Person domain model class
+/// </summary>
+public class Person
+{
+    [Key]
+    public Guid? Id { get; set; }
+
+    [StringLength(40)]
+    public string? Name { get; set; }
+
+    [StringLength(40)]
+    public string? Email { get; set; }
+
+    public DateTime? DateOfBirth { get; set; }
+
+    [StringLength(10)]
+    public string? Gender { get; set; }
+
+    [StringLength(200)]
+    public string? Address { get; set; }
+
+    public bool ReceiveNewsLetters { get; set; }
+
+    public string? TIN { get; set; }
+
+    public Guid? CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    public virtual Country? Country { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, " +
+            $"Name: {Name}, " +
+            $"Email: {Email}, " +
+            $"DateOfBirth: {DateOfBirth.Value.ToString("MM/dd/yyyy")}" +
+            $"Gender: {Gender}, " +
+            $"Address: {Address}, " +
+            $"ReceiveNewsLetters: {ReceiveNewsLetters}, " +
+            $"TIN: {TIN}, " +
+            $"CountryId: {CountryId}, " +
+            $"CountryName: {Country?.Name}";
+    }
+}
