300d67f51da63afa8dee5c612f46ef769db06a1b
Yosendra <yosendra.evri@gmail.com>
2024-02-09 06:21:48 +0800

    Identity & Security - 03. Register View

---
+++ 24. Identity & Security/03. Register View/ContactManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using ContactManager.Core.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.UI.Controllers;
+
+[Route("[controller]/[action]")]
+public class AccountController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(RegisterDto registerDto)
+    {
+        // To do: Store user registration details into Identity database
+        return RedirectToAction("Index", "Person");
+    }
+}
---
+++ 24. Identity & Security/03. Register View/ContactManager.UI/Program.cs
@@ -0,0 +1,45 @@
+﻿using CRUDExample;
+using CRUDExample.Middleware;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Register View
+* 
+* Add RegisterDto class at Dto folder
+* Add Register.cshtml in View -> Account
+* Add AccountController
+* Add hyperlink in _Layout.cshtml
+* 
+* Look at: 
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.ConfigureServices(builder.Configuration);
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
