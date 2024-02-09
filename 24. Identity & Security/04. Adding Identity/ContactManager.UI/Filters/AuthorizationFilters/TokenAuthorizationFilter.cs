9f36c9a37cf7fd3af431db3c53b55c987d8f9200
Yosendra <yosendra.evri@gmail.com>
2024-02-09 17:28:53 +0800

    Identity & Security - 04. Adding Identity

---
+++ 24. Identity & Security/04. Adding Identity/ContactManager.UI/Filters/AuthorizationFilters/TokenAuthorizationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.AuthorizationFilters;
+
+public class TokenAuthorizationFilter : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // Verify is token submitted in the form of a cookie as a part of the request
+        if (!context.HttpContext.Request.Cookies.ContainsKey("Auth-Key"))
+        {
+            // Unauthorized, Short-circuit the pipeline
+            context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+            return;
+        }
+
+        if (context.HttpContext.Request.Cookies["Auth-Key"] != "A100")
+        {
+            // Unauthorized, Short-circuit the pipeline
+            context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+            return;
+        }
+    }
+}
---
+++ 24. Identity & Security/04. Adding Identity/ContactManager.UI/Program.cs
@@ -0,0 +1,42 @@
+﻿using CRUDExample;
+using CRUDExample.Middleware;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Adding Identity
+* 
+* Adding Identity to IoC container in ConfigureServicesExtension class
+* 
+* Look at: ConfigureServicesExtension.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.ConfigureServices(builder.Configuration);
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
