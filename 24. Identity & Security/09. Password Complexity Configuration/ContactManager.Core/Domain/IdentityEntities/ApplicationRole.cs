26577f2c2ca19bdcd5da4a6e2dd1adde9fa437a2
Yosendra <yosendra.evri@gmail.com>
2024-02-10 03:50:13 +0800

    Identity & Security - 09. Password Complexity Configuration

---
+++ 24. Identity & Security/09. Password Complexity Configuration/ContactManager.Core/Domain/IdentityEntities/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ContactManager.Core.Domain.IdentityEntities;
+
+public class ApplicationRole : IdentityRole<Guid>
+{
+
+}
---
+++ 24. Identity & Security/09. Password Complexity Configuration/ContactManager.UI/Program.cs
@@ -0,0 +1,43 @@
+﻿using CRUDExample;
+using CRUDExample.Middleware;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Password Complexity Configuration
+* 
+* We can configure the password complexity when injecting Identity service
+* 
+* Look at: ConfigureServicesExtension.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.ConfigureServices(builder.Configuration);
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseRouting();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
