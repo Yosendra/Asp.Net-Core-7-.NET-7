6195cc3e8b16372ecaf98201a143e07037cfde7c
Yosendra <yosendra.evri@gmail.com>
2024-02-11 02:31:10 +0800

    Identity & Security - 12. ReturnUrl

---
+++ 24. Identity & Security/12. ReturnUrl/ContactManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using ContactManager.Core.Domain.IdentityEntities;
+using ContactManager.Core.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.UI.Controllers;
+
+[AllowAnonymous]
+[Route("[controller]/[action]")]
+public class AccountController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(
+        UserManager<ApplicationUser> userManager, 
+        SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterDto registerDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Errors = ModelState.Values.SelectMany(m => m.Errors).Select(e => e.ErrorMessage);
+            return View(registerDto);
+        }
+
+        ApplicationUser user = new()
+        {
+            Email = registerDto.Email,
+            UserName = registerDto.Email,
+            PersonName = registerDto.PersonName,
+            PhoneNumber = registerDto.Phone,
+        };
+
+        IdentityResult result = await _userManager.CreateAsync(user, registerDto.Password);
+        if (result.Succeeded)
+        {
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            return RedirectToAction("Index", "Person");
+        }
+        else
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("Register", error.Description);
+
+            return View(registerDto);
+        }
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginDto loginDto, string? returnUrl)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Errors = ModelState.Values.SelectMany(m => m.Errors).Select(e => e.ErrorMessage);
+            return View(loginDto);
+        }
+
+        string? userName = loginDto.Email;
+        string? password = loginDto.Password;
+        var result = await _signInManager.PasswordSignInAsync(userName, password, false, false);
+        if (result.Succeeded)
+        {
+            if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))  // this is to prevent cross site posting, direct to another domain
+                return LocalRedirect(returnUrl);                                // local means in the same domain, the same application
+
+            return RedirectToAction("Index", "Person");
+        }
+        else
+        {
+            ModelState.AddModelError("Login", "Invalid email or password");
+            return View(loginDto);
+        }
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Person");
+    }
+}
---
+++ 24. Identity & Security/12. ReturnUrl/ContactManager.UI/Program.cs
@@ -0,0 +1,48 @@
+﻿using CRUDExample;
+using CRUDExample.Middleware;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* ReturnUrl
+* 
+* In Login.cshtml we attach the ReturnUrl from query parameter in the form post
+* In the Login (POST) action method of AccountController, add parameter ReturnUrl
+* 
+* Add some logic to redirect when logic successfully 
+* if returnUrl exist and is local url then redirect to the mentioned url at Login (POST) action method in AccountController
+* 
+* Look at: Login.cshtml, AccountController.cs
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.ConfigureServices(builder.Configuration);
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
+public partial class Program {}
