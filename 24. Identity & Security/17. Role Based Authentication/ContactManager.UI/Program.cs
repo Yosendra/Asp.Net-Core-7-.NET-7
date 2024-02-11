9a748e7f619549bfd9862b32ea59105ce835760e
Yosendra <yosendra.evri@gmail.com>
2024-02-12 01:19:15 +0800

    Identity & Security - 17. Role Based Authentication

---
+++ 24. Identity & Security/17. Role Based Authentication/ContactManager.UI/Program.cs
@@ -0,0 +1,61 @@
+﻿using CRUDExample;
+using CRUDExample.Middleware;
+using Rotativa.AspNetCore;
+using Serilog;
+
+/* Role Based Authentication
+* 
+* User-role defines type of the user that has access to specific resources of the application.
+* Ex: Administrator role, Customer role, etc
+*             has access to
+* Administrator --------> Page, Page, Page
+*        
+*             has access to
+* Customer -------------> Page, Page, Page
+* 
+* We can use pre-defined filter [Authorize]
+* In HomeController (Admin) we apply this filter for Admin
+* 
+* Test: Login as User, then go to this path "/admin", you can't access the page
+* 
+* Look at: HomeController.cs (Admin)
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(service);
+});
+builder.Services.ConfigureServices(builder.Configuration);
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandlingMiddleware();
+}
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+if (!app.Environment.IsEnvironment("IntegrationTest"))
+    RotativaConfiguration.Setup("wwwroot");
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}");
+});
+app.Run();
+
+public partial class Program {}
