97350a6c3dbd8869dbc4a3ab699e999bf3a8c33b
Yosendra <yosendra.evri@gmail.com>
2024-02-13 03:56:56 +0800

    Web API - 03. Web API Controllers

---
+++ 25. ASP.NET Core Web API/03. Web API Controllers/CityManager.WebApi/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityManager.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    public string Get()             // this get invoked when request from browser go to this "/api/test/" path
+    {                               // the method's name is must be "Get" for GET request
+        return "Hello World";
+    }
+
+    [HttpGet]
+    public string SomeMethodName()  // or if you want different name for the action method, you can apply [HttpGet] attribute
+    {                               // on that named method like this
+        return "Hello World!!!";
+    }
+}
---
+++ 25. ASP.NET Core Web API/03. Web API Controllers/CityManager.WebApi/Program.cs
@@ -0,0 +1,46 @@
+
+/* Web API Controllers
+* 
+* Controller
+*   [ApiController]
+*   class ClassNameController
+*   {
+*     // action methods here
+*   }
+* 
+* Should be either or both:
+* • The class name should be suffixed with "Controller". Eg: ProductController
+* • The [ApiController] attribute is applied to the same class or its base class
+* 
+* Optional:
+* • Is a public class
+* • Inherited from Microsoft.AspNetCore.Mvc.ControllerBase
+* 
+* 
+* Add TestController. Right click on Cotrollers folder, then add controller.
+* In category, choose API, not MVC, then choose "API Controller - Empty"
+* 
+* When seeing path like "api/[controller]", it means it only return data (like json or xml, indicated by "api" in url path). 
+* It is convention to indicate it is a Web API url.
+* 
+* In launchSettings.json, in "launchUrl" key, on both "http" and "https" profile, change the value to "api/test"
+* It means to make the path "api/test" is the default path we are going when first run the application.
+* 
+* Add the action method for TestController.
+* Run the application, the look at the address bar, also put breakpoint to test it
+* 
+* Look at: WeatherForecastController.cs, launchSettings.json
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
