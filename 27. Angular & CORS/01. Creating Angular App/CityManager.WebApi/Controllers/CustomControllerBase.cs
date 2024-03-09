4869d5a79752da5ebdf9a11996b596903f84d86a
Yosendra <yosendra.evri@gmail.com>
2024-03-10 05:44:50 +0800

    Delete files

--- 27. Angular & CORS/01. Creating Angular App/CityManager.WebApi/Controllers/CustomControllerBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace CityManager.WebApi.Controllers;
-
-[Route("api/v{version:apiVersion}/[controller]")]
-[ApiController]
-public class CustomControllerBase : ControllerBase
-{
-}
